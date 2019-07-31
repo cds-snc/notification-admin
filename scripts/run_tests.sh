a2a2488587402486ebf61aa55dc534731096d8fe
Max <max@neuvians.net>
2019-07-31 11:19:38 -0400

    Printf fixes

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,24 @@
-cat app/version.py
+#!/bin/sh
+#
+# Run project tests
+#
+# NOTE: This script expects to be run from the project root with
+# ./scripts/run_tests.sh
+
+set -o pipefail
+
+function display_result {
+  RESULT=$1
+  EXIT_STATUS=$2
+  TEST=$3
+
+  if [ $RESULT -ne 0 ]; then
+    echo -e "\033[31m$TEST failed\033[0m"
+    exit $EXIT_STATUS
+  else
+    echo -e "\033[32m$TEST passed\033[0m"
+  fi
+}
 
 # make test-requirements
 display_result $? 1 "Requirements check"
