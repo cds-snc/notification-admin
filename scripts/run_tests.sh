d37b35454010d56f39cbfc6cb75631851ec520c5
Max <max@neuvians.net>
2019-07-31 10:36:03 -0400

    Test version output

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,24 +1,4 @@
-#!/bin/sh
-#
-# Run project tests
-#
-# NOTE: This script expects to be run from the project root with
-# ./scripts/run_tests.sh
-
-set -o pipefail
-
-function display_result {
-  RESULT=$1
-  EXIT_STATUS=$2
-  TEST=$3
-
-  if [ $RESULT -ne 0 ]; then
-    echo -e "\033[31m$TEST failed\033[0m"
-    exit $EXIT_STATUS
-  else
-    echo -e "\033[32m$TEST passed\033[0m"
-  fi
-}
+cat app/version.py
 
 # make test-requirements
 display_result $? 1 "Requirements check"
