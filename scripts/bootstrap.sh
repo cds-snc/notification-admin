7be76d2f764f65b9a5d7786caa51f36eafb87a02
Max Neuvians <max@neuvians.net>
2019-07-31 12:00:57 -0400

    Test heroku (#97)
    * Test heroku
    
    * Update runtime
    
    * Exact version
    
    * Fixed heroku sh permission
    
    * Added npm buildpack
    
    * Mods for procfile
    
    * Moved generate version fike;
    
    * Fix version file generator
    
    * Get app URL
    
    * Test version output
    
    * Different echo
    
    * Printf instead of echo
    
    * Printf fixes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,21 +5,6 @@
 # NOTE: This script expects to be run from the project root with
 # ./scripts/bootstrap.sh
 
-set -o pipefail
-
-function display_result {
-  RESULT=$1
-  EXIT_STATUS=$2
-  TEST=$3
-
-  if [ $RESULT -ne 0 ]; then
-    echo -e "\033[31m$TEST failed\033[0m"
-    exit $EXIT_STATUS
-  else
-    echo -e "\033[32m$TEST passed\033[0m"
-  fi
-}
-
 # we need the version file to exist otherwise the app will blow up
 make generate-version-file
 
--- scripts/heroku.sh
+++
@@ -1,4 +0,0 @@
-# we need the version file to exist otherwise the app will blow up
-make generate-version-file
-
-npm install && npm run build
