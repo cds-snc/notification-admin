6edfece5424726e7080217fa498f0a56b00b87f1
Max Neuvians <max@neuvians.net>
2019-08-02 14:44:28 -0400

    Added dynamic time display (#110)
    * Added dynamic time display
    
    * JS lint

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,33 @@ $(() => $("time.timeago").timeago());
 $(() => GOVUK.stickAtTopWhenScrolling.init());
 $(() => GOVUK.stickAtBottomWhenScrolling.init());
 
+$(".local-datetime-short").each(function(index) {
+  let time = moment($(this).text().trim());
+  if(time.isValid()){
+    $(this).text(time.format("D MMM, kk:mm a"));
+  }
+});
+
+$(".local-datetime-full").each(function(index) {
+    let time = moment($(this).text().trim());
+    if(time.isValid()){
+      $(this).text(time.format("LLL"));
+    }
+});
+
+$(".relative-time-future").each(function(index) {
+  let time = moment($(this).text().trim());
+  if(time.isValid()){
+    $(this).text(time.toNow());
+  }
+});
+
+$(".relative-time-past").each(function(index) {
+  let time = moment($(this).text().trim());
+  if(time.isValid()){
+    $(this).text(time.fromNow());
+  }
+});
 
 var showHideContent = new GOVUK.ShowHideContent();
 showHideContent.init();
