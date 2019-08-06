e1ca706905874b62809dc909734dd267f279e5fe
Max Neuvians <max@neuvians.net>
2019-08-06 09:31:42 -0400

    Added sentry integration (#113)
    * Added sentry integration
    
    * Sort order
    
    * Fix to script

--- application.py
+++ application.py
@@ -1,8 +1,18 @@
+import os
+
+import sentry_sdk
 from dotenv import load_dotenv
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 from app import create_app
 
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_URL', ''),
+    integrations=[FlaskIntegration()]
+)
 
 application = Flask('app')
 
