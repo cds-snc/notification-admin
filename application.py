d832d9fe90bdf05aef0f4f1875b2ab64887b1978
Max <max@neuvians.net>
2019-08-06 08:46:57 -0400

    Added sentry integration

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
 from dotenv import load_dotenv
 from flask import Flask
 from app import create_app
+import sentry_sdk
+import os
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_URL', ''),
+    integrations=[FlaskIntegration()]
+)
 
 application = Flask('app')
 
