c81bcb043467563d0d8b88c53655d614c3e7c588
Tim Arney <tim@line37.com>
2019-08-21 13:51:14 -0400

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,6 @@ const javascripts = () => {
       paths.src + 'javascripts/highlightTags.js',
       paths.src + 'javascripts/fileUpload.js',
       paths.src + 'javascripts/expandCollapse.js',
-      paths.src + 'javascripts/radioSelect.js',
       paths.src + 'javascripts/updateContent.js',
       paths.src + 'javascripts/listEntry.js',
       paths.src + 'javascripts/liveSearch.js',
