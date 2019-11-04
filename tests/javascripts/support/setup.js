640d0a7b46a18119d73290fff8e9a13b7fdbb4c1
smcmurtry <smcmurtry@gmail.com>
2019-11-04 15:58:12 -0700

    added translations to jest setup script

--- tests/javascripts/support/setup.js
+++ tests/javascripts/support/setup.js
@@ -1,6 +1,22 @@
+
+
 // set up jQuery
 window.jQuery = require('jquery');
 $ = window.jQuery;
 
 // load module code
 require('govuk_frontend_toolkit/javascripts/govuk/modules.js');
+
+
+// setting up translations
+window.APP_LANG = "en"
+window.APP_PHRASES = {
+    copied_to_clipboard: "Copied to clipboard",
+    show: "Show",
+    copy: "Copy",
+    to_clipboard: "to clipboard"
+}
+
+var Polyglot = require("node-polyglot");
+
+window.polyglot = new Polyglot({ phrases: APP_PHRASES, locale: APP_LANG });
