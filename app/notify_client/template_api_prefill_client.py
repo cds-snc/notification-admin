b77fa09a589b700645b82758a4ee6f7a93dc2cd4
Tim Arney <tim@line37.com>
2019-11-14 18:15:09 -0500

    email-perms

--- app/notify_client/template_api_prefill_client.py
+++ app/notify_client/template_api_prefill_client.py
@@ -1,25 +1,26 @@
 from notifications_python_client.errors import HTTPError
-
 from app.notify_client import NotifyAdminAPIClient
 
-
 class TemplateApiPrefillClient(NotifyAdminAPIClient):
-
     def init_app(self, app):
         super().init_app(app)
-        self.service_id = app.config['NOTIFY_TEMPLATE_PREFILL_SERVICE_ID']
-    
+        self.prefill_service_id = app.config['NOTIFY_TEMPLATE_PREFILL_SERVICE_ID']
+
     def get_template_list(self):
-        endpoint = '/service/{}/template'.format(self.service_id)
-        return self.get(url=endpoint)["data"]
+        try:
+            endpoint = '/service/{}/template'.format(self.prefill_service_id)
+            return self.get(url=endpoint)["data"]
+        except HTTPError as e:
+            print(e.message)
+            return False
 
     def get_template(self, template_id):
         try:
-            endpoint = '/service/{}/template/{}'.format(self.service_id, template_id)
+            endpoint = '/service/{}/template/{}'.format(self.prefill_service_id, template_id)
             result = self.get(url=endpoint)
             return result["data"]
         except HTTPError as e:
-            return {"result": "error", "error": e.message}
-
+            print(e.message)
+            return False
 
 template_api_prefill_client = TemplateApiPrefillClient()
