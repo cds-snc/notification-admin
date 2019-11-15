38bf9bebd03a2ce053711fe5d5af22de4f660e54
Tim Arney <tim@line37.com>
2019-11-15 12:05:10 -0500

    bad link

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -25,12 +25,8 @@ from app.utils import get_logo_cdn_domain, user_is_logged_in
 QUESTION_TICKET_TYPE = 'ask-question-give-feedback'
 
 
-@main.route('/&nbsp;', methods=['GET'])
-def badindex():
-    return redirect(url_for('main.index'), 301)
-
-
 @main.route('/', methods=['GET', 'POST'])
+@main.route('/&nbsp;', methods=['GET'])
 def index():
 
     if current_user and current_user.is_authenticated:
