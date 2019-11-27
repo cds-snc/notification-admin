57389aff6a405972f93f43e16ae23e37ab81871f
Tim Arney <tim@line37.com>
2019-11-27 09:22:21 -0500

    add code style

--- tests/app/main/views/test_dashboard.py
+++ tests/app/main/views/test_dashboard.py
@@ -577,23 +577,6 @@ def test_monthly_shows_letters_in_breakdown(
     assert normalize_spaces(columns[1].text) == 'text messages'
 
 
-@pytest.mark.parametrize('endpoint', [
-    'main.monthly',
-    'main.template_usage',
-])
-@freeze_time("2015-01-01 15:15:15.000000")
-def test_stats_pages_show_last_3_years(
-    client_request,
-    endpoint,
-    mock_get_monthly_notification_stats,
-    mock_get_monthly_template_usage,
-):
-    page = client_request.get(
-        endpoint,
-        service_id=SERVICE_ONE_ID,
-    )
-
-
 def test_monthly_has_equal_length_tables(
     client_request,
     service_one,
