ab13ec763122e4bd11ce98f12776ae758f5518d7
Tim Arney <tim@line37.com>
2020-03-03 10:51:20 -0500

    sync

--- tests/app/main/views/test_index.py
+++ tests/app/main/views/test_index.py
@@ -149,6 +149,7 @@ def test_css_is_served_from_correct_path(client_request):
     ):
 
         assert link['href'].startswith([
+            'https://static.example.com/stylesheets/header.css?',
             'https://static.example.com/stylesheets/notification-template.css?',
             'https://static.example.com/stylesheets/main.css?',
         ][index])
