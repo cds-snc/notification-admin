5896f8cd874d5f4cbac9b66743bff536790a2b93
Tim Arney <tim@line37.com>
2020-03-18 14:04:34 -0400

    fix

--- app/main/views/sign_out.py
+++ app/main/views/sign_out.py
@@ -1,11 +1,17 @@
-from flask import redirect, session, url_for
+from flask import redirect, session, url_for, current_app
 from flask_login import logout_user
 
+from app import (
+    get_current_locale
+)
 from app.main import main
 
 
 @main.route('/sign-out', methods=(['GET']))
 def sign_out():
+    currentlang = get_current_locale(current_app)
     session.clear()
     logout_user()
+    session["userlang"] = currentlang
+
     return redirect(url_for('main.index'))
