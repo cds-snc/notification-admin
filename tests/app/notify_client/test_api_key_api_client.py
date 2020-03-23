03f313e343a17d114e9c7067e5feb443c8661f03
smcmurtry <smcmurtry@gmail.com>
2020-03-23 00:05:10 -0600

    fixed endpoint name in test

--- tests/app/notify_client/test_api_key_api_client.py
+++ tests/app/notify_client/test_api_key_api_client.py
@@ -5,7 +5,7 @@ from app.notify_client.api_key_api_client import ApiKeyApiClient
 
 def test_get_api_key_statistics(mocker, api_user_active):
     api_key_id = uuid.uuid4()
-    expected_url = '/api-key/{}/total-sends'.format(api_key_id)
+    expected_url = '/api-key/{}/summary-statistics'.format(api_key_id)
     client = ApiKeyApiClient()
 
     mock_get = mocker.patch('app.notify_client.api_key_api_client.ApiKeyApiClient.get')
