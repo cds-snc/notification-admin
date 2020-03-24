1923fc6b53fdc3fb0bdd4a9a5f6e322394336cf0
smcmurtry <smcmurtry@gmail.com>
2020-03-23 21:15:32 -0600

    fixed mock data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1826,7 +1826,13 @@ def mock_get_no_api_keys(mocker):
 @pytest.fixture(scope='function')
 def mock_get_api_key_statistics(mocker):
     def _get_stats(key_id):
-        data = {'total_sends': 20, 'api_key_id': key_id}
+        data = {
+            'total_sends': 20, 
+            'api_key_id': key_id, 
+            'email_sends': 20,
+            'sms_sends': 0,
+            'last_send': '2020-03-16T17:32:32.883281Z'
+        }
         return data
 
     return mocker.patch('app.api_key_api_client.get_api_key_statistics', side_effect=_get_stats)
