d4dc3281f9b12fd42986e6f6248132a5bd941fc8
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2020-03-25 12:08:07 -0600

    Functional send now / send later buttons as Javascript

--- app/assets/javascripts/scheduler/SetDateTime/SetDateTime.js
+++ app/assets/javascripts/scheduler/SetDateTime/SetDateTime.js
@@ -3,8 +3,6 @@ import { store } from "./index";
 
 const setDateAndTimeValue = val => {
   const ref = document.getElementById("scheduled_for");
-  
-  console.log("hook")
 
   let value = val;
 
@@ -23,9 +21,19 @@ const setDateAndTimeValue = val => {
 export const SetDateTime = () => {
   const { selected: selectedDate, time } = useContext(store);
 
-  if (!time) {
+  if (!time || document.getElementById("schedule-send-at").classList.contains("hidden")) {
     setDateAndTimeValue("");
     return null;
+  }
+
+  // allow radio buttons to control visibility of calendar / 'send now' value
+  document.getElementById("send_now_later").onchange = (e) => {
+    if(e.target.value === "1") {
+      setDateAndTimeValue("");
+      document.getElementById("schedule-send-at").classList.add("hidden");
+    } else {
+      document.getElementById("schedule-send-at").classList.remove("hidden");
+    }
   }
 
   setDateAndTimeValue(`${selectedDate[0]} ${time}`);
