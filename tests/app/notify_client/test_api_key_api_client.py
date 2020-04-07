994adaca11569c1c6721868485ff1245d3eb1edd
smcmurtry <smcmurtry@gmail.com>
2020-04-06 21:53:27 -0600

    fixed the navigation test

--- tests/app/notify_client/test_api_key_api_client.py
+++ tests/app/notify_client/test_api_key_api_client.py
@@ -12,3 +12,13 @@ def test_get_api_key_statistics(mocker, api_user_active):
 
     client.get_api_key_statistics(api_key_id)
     mock_get.assert_called_once_with(url=expected_url)
+
+def test_get_api_keys_ranked(mocker, api_user_active):
+    n_days_back = 2
+    expected_url = '/api-key/ranked-by-notifications-created/{}'.format(n_days_back)
+    client = ApiKeyApiClient()
+
+    mock_get = mocker.patch('app.notify_client.api_key_api_client.ApiKeyApiClient.get')
+
+    client.get_api_keys_ranked_by_notifications_created(n_days_back)
+    mock_get.assert_called_once_with(url=expected_url)
