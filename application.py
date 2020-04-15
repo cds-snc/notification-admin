be7b56930127108697d9cec4d78818c2865d7d66
Max Neuvians <max@neuvians.net>
2020-04-15 16:13:07 -0400

    Add honeycomb middleware

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 import os
 
+import beeline
 import sentry_sdk
+from beeline.middleware.flask import HoneyMiddleware
 from dotenv import load_dotenv
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
@@ -15,7 +17,14 @@ sentry_sdk.init(
     integrations=[FlaskIntegration()]
 )
 
+beeline.init(
+    writekey=os.environ.get('HONEYCOMB_API_KEY', ''),
+    dataset='notification',
+    service_name='notification-admin'
+)
+
 application = Flask('app')
 application.wsgi_app = ProxyFix(application.wsgi_app)
+application = HoneyMiddleware(application)
 
 create_app(application)
