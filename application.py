f9a2d98e2ddfe952ff95b4d9ccb71eb7beb616f5
Max Neuvians <max@neuvians.net>
2020-04-22 09:55:54 -0400

    Fix unused import

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ import os
 import sentry_sdk
 from dotenv import load_dotenv
 from flask import Flask
-from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
