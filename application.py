962d409b5b73fdeb25b20e5cd00e564cd3b99b32
Max Neuvians <max@neuvians.net>
2020-04-22 09:50:26 -0400

    Better label

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
-    release="notify-admin" + os.environ.get('GIT_SHA', '')
+    release="notify-admin@" + os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
