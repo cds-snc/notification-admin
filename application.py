20f6ad40e27ca4e22a7621cc23be66313090d5ee
Max Neuvians <max@neuvians.net>
2020-04-22 13:58:43 -0400

    Sentry release (#465)
    * Improve sentry tagging
    
    * Better label
    
    * Fix unused import

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ import os
 import sentry_sdk
 from dotenv import load_dotenv
 from flask import Flask
-from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
@@ -12,8 +11,7 @@ load_dotenv()
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
-    integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release="notify-admin@" + os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
