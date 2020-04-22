7f394fc40bf18c85037cc88c2e4b1f6cdaf9d129
Max Neuvians <max@neuvians.net>
2020-04-22 09:49:45 -0400

    Improve sentry tagging

--- application.py
+++ application.py
@@ -12,8 +12,7 @@ load_dotenv()
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
-    integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release="notify-admin" + os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
