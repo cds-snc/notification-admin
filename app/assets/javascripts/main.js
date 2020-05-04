35686d7a3cdf8e350a9bc633d66457f1acb7385d
smcmurtry <smcmurtry@gmail.com>
2020-05-04 15:27:12 -0600

    got it working with the timeago jquery library

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -47,7 +47,48 @@ window.formatAllDates = function () {
       $(this).text(`${dayStr}, ${timeStr}`);
     }
   });
+  
+  // fr from here: https://github.com/rmm5t/jquery-timeago/blob/master/locales/jquery.timeago.fr.js
+  // en from here: https://github.com/rmm5t/jquery-timeago/blob/master/locales/jquery.timeago.en.js
+  // I removed "about"/"environ" to match the existing language returned by the python ago package
+  let timeAgoSettings = {
+    fr: {
+      prefixAgo: "il y a",
+      prefixFromNow: "d'ici",
+      seconds: "moins d'une minute",
+      minute: "une minute",
+      minutes: "%d minutes",
+      hour: "une heure",
+      hours: "%d heures",
+      day: "un jour",
+      days: "%d jours",
+      month: "un mois",
+      months: "%d mois",
+      year: "un an",
+      years: "%d ans"
+    },
+    en: {
+      prefixAgo: null,
+      prefixFromNow: null,
+      suffixAgo: "ago",
+      suffixFromNow: "from now",
+      seconds: "less than a minute",
+      minute: "a minute",
+      minutes: "%d minutes",
+      hour: "an hour",
+      hours: "%d hours",
+      day: "a day",
+      days: "%d days",
+      month: "a month",
+      months: "%d months",
+      year: "a year",
+      years: "%d years",
+      wordSeparator: " ",
+      numbers: []
+    }
+  };
 
+  window.jQuery.timeago.settings.strings = timeAgoSettings[window.APP_LANG];
   $(() => $("time.timeago").timeago());
 
 }
