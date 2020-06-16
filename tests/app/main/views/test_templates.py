3d0b986a9e2afc5a58bd29301fe4ccccb302dada
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2020-06-16 11:41:01 -0400

    attempt to fix tests

--- tests/app/main/views/test_templates.py
+++ tests/app/main/views/test_templates.py
@@ -426,7 +426,7 @@ def test_user_with_only_send_and_view_sees_letter_page(
         'Templates Two week reminder'
     )
     assert normalize_spaces(page.select_one('title').text) == (
-        'Two week reminder – Templates – service one – GC Notify'
+        'Two week reminder – Templates – service one'
     )
 
 
@@ -622,7 +622,7 @@ def test_should_be_able_to_view_a_template_with_links(
         'Templates Two week reminder'
     )
     assert normalize_spaces(page.select_one('title').text) == (
-        'Two week reminder – Templates – service one – GC Notify'
+        'Two week reminder – Templates – service one'
     )
 
     links_in_page = page.select('.pill-separate-item')
