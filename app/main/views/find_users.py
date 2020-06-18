ee9d4f88e45bb5b7e3f3b6c730f14b1ef1327b30
Steve Astels <sastels@users.noreply.github.com>
2020-06-18 12:35:01 -0400

    add translation (#619)

--- app/main/views/find_users.py
+++ app/main/views/find_users.py
@@ -1,4 +1,5 @@
 from flask import flash, redirect, render_template, request, url_for
+from flask_babel import _
 from flask_login import current_user
 
 from app import user_api_client
@@ -40,7 +41,7 @@ def block_user(user_id):
         user.update(blocked=True, updated_by=current_user.id)
         return redirect(url_for('.user_information', user_id=user_id))
     else:
-        flash(['Are you sure you want to block this user?'], 'block')
+        flash([_('Are you sure you want to block this user?')], 'block')
         return user_information(user_id)
 
 
@@ -52,7 +53,7 @@ def unblock_user(user_id):
         user.update(blocked=False, updated_by=current_user.id)
         return redirect(url_for('.user_information', user_id=user_id))
     else:
-        flash(['Are you sure you want to unblock this user?'], 'unblock')
+        flash([_('Are you sure you want to unblock this user?')], 'unblock')
         return user_information(user_id)
 
 
@@ -65,5 +66,5 @@ def archive_user(user_id):
 
         return redirect(url_for('.user_information', user_id=user_id))
     else:
-        flash('There\'s no way to reverse this! Are you sure you want to archive this user?', "archive")
+        flash(_("There's no way to reverse this! Are you sure you want to archive this user?"), "archive")
         return user_information(user_id)
