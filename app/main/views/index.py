c223d201cd65dae2fd42b22b3689fb0849468f35
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-20 15:01:17 -0500

    Inject admin_base_url in signedout.html

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -59,6 +59,7 @@ def index():
             'views/signedout.html',
             form=form,
             scrollTo="true",
+            admin_base_url=current_app.config['ADMIN_BASE_URL'],
             stats=get_latest_stats(get_current_locale(current_app)),
         )
 
@@ -66,6 +67,7 @@ def index():
         'views/signedout.html',
         form=form,
         scrollTo="false",
+        admin_base_url=current_app.config['ADMIN_BASE_URL'],
         stats=get_latest_stats(get_current_locale(current_app)),
     )
 
