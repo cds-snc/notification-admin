552d1022f13bcbb70a34ece9d53424072eeb8573
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2020-12-16 12:00:44 -0700

    fix home page bugs

--- postcss.config.js
+++ postcss.config.js
@@ -26,6 +26,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
     "w-2/3",
     "mb-8",
     "focus:outline-none",
+    "w-3/6",
   ],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
