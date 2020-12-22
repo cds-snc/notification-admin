0294af08b4c092ddd218383d816ba6186ba88197
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-12-22 03:09:00 -0500

    Revamp contact form with multiple steps (#854)
    * WIP
    
    * Imports
    
    * refactor page_footer
    
    * Translations
    
    * Submit logic
    
    * Minor improvements
    
    * Rebuild CSS
    
    * Comment unused variables
    
    * Translate homepage
    
    * Work on tests
    
    * More assertions
    
    * Add test for success case for demo
    
    * Test back links and redirects
    
    * Improve code style
    
    * Test validation for demo funnel
    
    * Refactor
    
    * New shade of blue for homepage
    
    * Sort imports
    
    * Remove some padding
    
    * New color name
    
    * Remove padding bottom
    
    * Enable autocomplete on identity step
    
    * Fix contact form button
    
    * No margin bottom
    
    * Use more Tailwind classes
    
    * Sensible max length values
    
    * Use constants
    
    * Test contact page works while logged out
    
    * Fix autocomplete attributes
    
    * Update content string for steps
    
    * Update content on step 2

--- app/main/views/contact.py
+++ app/main/views/contact.py
@@ -1,23 +1,157 @@
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, request, session, url_for
+from flask_babel import lazy_gettext as _l
+from flask_login import current_user
 
 from app import user_api_client
 from app.main import main
-from app.main.forms import ContactNotifyTeam
+from app.main.forms import (
+    ContactMessageStep,
+    ContactNotify,
+    SetUpDemoOrgDetails,
+    SetUpDemoPrimaryPurpose,
+)
+
+DEFAULT_STEP = "identity"
+SESSION_FORM_KEY = "contact_form"
+SESSION_FORM_STEP_KEY = "contact_form_step"
+steps = [
+    {
+        "form": ContactNotify,
+        "current_step": DEFAULT_STEP,
+        "previous_step": None,
+        "next_step": "message",
+    },
+    {
+        "form": ContactMessageStep,
+        "current_step": "message",
+        "previous_step": DEFAULT_STEP,
+        "next_step": None,
+        "support_types": ['ask_question', 'technical_support', 'give_feedback', 'other'],
+    },
+    {
+        "form": SetUpDemoOrgDetails,
+        "current_step": "set_up_demo.org_details",
+        "previous_step": DEFAULT_STEP,
+        "next_step": "set_up_demo.primary_purpose",
+        "support_types": ["demo"],
+        "step": 1,
+        "total_steps": 2,
+    },
+    {
+        "form": SetUpDemoPrimaryPurpose,
+        "current_step": "set_up_demo.primary_purpose",
+        "previous_step": "set_up_demo.org_details",
+        "next_step": None,
+        "step": 2,
+        "total_steps": 2,
+    },
+]
 
 
 @main.route('/contact', methods=['GET', 'POST'])
 def contact():
-    form = ContactNotifyTeam()
+    current_step = request.args.get('current_step', session.get(SESSION_FORM_STEP_KEY, DEFAULT_STEP))
+    try:
+        form_obj = [f for f in steps if f["current_step"] == current_step][0]
+    except IndexError:
+        return redirect(url_for('.contact', current_step=DEFAULT_STEP))
+    form = form_obj["form"](data=session.get(SESSION_FORM_KEY, {}))
 
-    if form.validate_on_submit():
-        # send email here
-        user_api_client.send_contact_email(form.name.data, form.email_address.data, form.feedback.data, form.support_type.data)
-
+    # Validating the final form
+    if form_obj["next_step"] is None and form.validate_on_submit():
+        session.pop(SESSION_FORM_KEY, None)
+        session.pop(SESSION_FORM_STEP_KEY, None)
+        send_form_to_freshdesk(form)
         return render_template('views/contact/thanks.html')
 
-    return render_template('views/contact/form.html', form=form)
+    # Going on to the next step in the form
+    if form.validate_on_submit():
+        possibilities = [f for f in steps if f["previous_step"] == current_step]
+        try:
+            if len(possibilities) == 1:
+                form_obj = possibilities[0]
+            else:
+                form_obj = [
+                    e for e in possibilities if form.support_type.data in e["support_types"]
+                ][0]
+        except IndexError:
+            return redirect(url_for('.contact', current_step=DEFAULT_STEP))
+        form = form_obj["form"](data=session.get(SESSION_FORM_KEY, {}))
+
+    session[SESSION_FORM_KEY] = form.data
+    session[SESSION_FORM_STEP_KEY] = form_obj["current_step"]
+
+    return render_template(
+        'views/contact/form.html',
+        form=form,
+        next_step=form_obj["next_step"],
+        current_step=form_obj["current_step"],
+        previous_step=form_obj["previous_step"],
+        step_hint=form_obj.get("step"),
+        total_steps_hint=form_obj.get("total_steps"),
+        **_labels(form_obj["previous_step"], form_obj["current_step"], form.support_type.data),
+    )
+
+
+def _labels(previous_step, current_step, support_type):
+    back_link = url_for('.contact', current_step=previous_step)
+    message_label = None
+
+    if current_step == "message":
+        if support_type == "other":
+            page_title = _l("Tell us more")
+            message_label = _l("Your message")
+        elif support_type == "give_feedback":
+            page_title = _l("Give feedback")
+            message_label = _l("How can we do better?")
+        elif support_type == "technical_support":
+            page_title = _l("Get technical support")
+            message_label = _l("Describe the problem.")
+        elif support_type == "ask_question":
+            page_title = _l("Ask a question")
+            message_label = _l("Your question")
+        else:
+            raise NotImplementedError
+    elif current_step in ["set_up_demo.org_details", "set_up_demo.primary_purpose"]:
+        page_title = _l("Set up a demo")
+    else:
+        page_title = _l("Contact us")
+        back_link = None
+
+    return {
+        "page_title": page_title,
+        "back_link": back_link,
+        "message_label": message_label,
+    }
+
+
+def send_form_to_freshdesk(form):
+    if form.support_type.data == "demo":
+        message = '<br><br>'.join([
+            f'- user: {form.name.data} {form.email_address.data}',
+            f'- department/org: {form.department_org_name.data}',
+            f'- program/service: {form.program_service_name.data}',
+            f'- intended_recipients: {form.intended_recipients.data}',
+            f'- main use case: {form.main_use_case.data}',
+            f'- main use case details: {form.main_use_case_details.data}',
+        ])
+    else:
+        message = form.message.data
+
+    if current_user and current_user.is_authenticated:
+        user_profile = url_for('.user_information', user_id=current_user.id, _external=True)
+        message += f"<br><br>---<br><br> {user_profile}"
+
+    friendly_support_type = str(dict(form.support_type.choices)[form.support_type.data])
+
+    user_api_client.send_contact_email(
+        form.name.data,
+        form.email_address.data,
+        message,
+        friendly_support_type
+    )
 
 
 @main.route('/support/ask-question-give-feedback', endpoint='redirect_contact')
-def redirect_contect():
+def redirect_contact():
     return redirect(url_for('main.contact'), code=301)
--- tests/app/main/test_contact.py
+++ tests/app/main/test_contact.py
@@ -1,130 +1,383 @@
 import pytest
+from flask import url_for
+from flask_login import current_user
 
-from app.main.forms import ContactNotifyTeam
 from tests.conftest import normalize_spaces
 
 
-@pytest.mark.parametrize('name, support_type, email_address, feedback, error_fields', [
-    ('John', 'Ask a question', 'john@example.com', '', ['feedback']),
-    ('John', 'Ask a question', 'john@example.com', 'Hello!', []),
-    ('John', 'Set up a demo', 'john@example.com', 'Demo plz!', []),
-    ('John', 'Give feedback', 'john@example.com', 'Yo, feedback', []),
-    ('John', 'Other', 'john@example.com', 'Stuff, you know', []),
-    ('', '', '', '', ['name', 'support_type', 'email_address', 'feedback']),
-])
-def test_form_fails_validation_without_required_fields(
-    app_,
-    name,
-    support_type,
-    email_address,
-    feedback,
-    error_fields
-):
-    data = {
-        'name': name,
-        'support_type': support_type,
-        'email_address': email_address,
-        'feedback': feedback,
-    }
-    with app_.test_request_context(method='POST', data=data):
-        form = ContactNotifyTeam()
-
-        if len(error_fields):
-            assert not form.validate_on_submit()
-        else:
-            assert form.validate_on_submit()
-
-        assert len(form.errors) == len(error_fields)
-
-        for field in error_fields:
-            assert form.errors[field] is not None
+def assert_no_back_link(page):
+    assert len(page.select('.back-link')) == 0
 
 
-def test_form_validation_fails_with_invalid_email_address_field(app_):
-    data = {
-        'name': 'John',
-        'support_type': 'Ask a question',
-        'email_address': 'nope',
-        'feedback': 'Awesome',
-    }
-
-    with app_.test_request_context(method='POST', data=data):
-        form = ContactNotifyTeam()
-
-        assert not form.validate_on_submit()
-        assert len(form.errors) == 1
-        assert form.errors['email_address'] == ['Enter a valid email address']
+def assert_has_back_link(page):
+    assert len(page.select('.back-link')) == 1
 
 
-def test_form_validation_fails_with_invalid_support_type_field(app_):
-    data = {
-        'name': 'John',
-        'support_type': 'Hello',
-        'email_address': 'john@example.com',
-        'feedback': 'Awesome',
-    }
-
-    with app_.test_request_context(method='POST', data=data):
-        form = ContactNotifyTeam()
-
-        assert not form.validate_on_submit()
-        assert len(form.errors) == 1
-        assert form.errors['support_type'] == ['Not a valid choice']
+def test_contact_page_does_not_require_login(client_request):
+    client_request.logout()
+    client_request.get('.contact')
 
 
-def test_submit_contact_form_validates(
-    client_request,
-    mocker,
-):
-    mock_send_contact_email = mocker.patch(
-        'app.user_api_client.send_contact_email'
-    )
-
+def test_identity_step_validates(client_request):
     page = client_request.post(
         '.contact',
         _expected_status=200,
         _data={
             'name': '',
-            'support_type': 'Ask a question',
-            'email_address': 'john@example.com',
-            'feedback': ''
+            'email_address': 'foo',
+            'support_type': ''
         }
     )
+
     assert [
         (error['data-error-label'], normalize_spaces(error.text))
         for error in page.select('.error-message')
     ] == [
         ('name', 'This cannot be empty'),
-        ('feedback', 'This cannot be empty'),
+        ('email_address', 'Enter a valid email address'),
+        ('support_type', 'You need to choose an option'),
     ]
-    assert mock_send_contact_email.called is False
+    assert_no_back_link(page)
 
 
-def test_submit_contact_form_sends_ticket(
-    client_request,
-    mocker,
-):
-    mock_send_contact_email = mocker.patch(
-        'app.user_api_client.send_contact_email'
+def test_back_link_goes_to_previous_step(client_request):
+    page = client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={
+            'name': 'John',
+            'email_address': 'john@example.com',
+            'support_type': 'other'
+        }
     )
 
-    data = {
-        'name': 'John',
-        'support_type': 'Ask a question',
-        'email_address': 'john@example.com',
-        'feedback': 'Awesome'
-    }
+    assert page.select_one('.back-link')['href'] == url_for('.contact', current_step='identity')
+
+    page = client_request.get_url(page.select_one('.back-link')['href'], _test_page_title=False)
+
+    # Fields have been saved and are filled
+    assert page.select_one('input[checked]')['value'] == 'other'
+    assert [
+        (input['name'], input['value'])
+        for input in page.select('input')
+        if input['name'] in ['name', 'email_address']
+    ] == [
+        ('name', 'John'),
+        ('email_address', 'john@example.com')
+    ]
+
+
+def test_invalid_step_name_redirects(client_request):
+    client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={
+            'name': 'John',
+            'email_address': 'john@example.com',
+            'support_type': 'other'
+        }
+    )
+
+    client_request.get_url(
+        url_for('.contact', current_step='nope'),
+        _expected_status=302,
+        _expected_redirect=url_for('.contact', current_step='identity', _external=True)
+    )
+
+
+@pytest.mark.parametrize('support_type', [
+    'ask_question', 'technical_support', 'give_feedback', 'other'
+])
+def test_message_step_valides(client_request, support_type, mocker):
+    mock_send_contact_email = mocker.patch('app.user_api_client.send_contact_email')
 
     page = client_request.post(
         '.contact',
         _expected_status=200,
-        _data=data
+        _data={
+            'name': 'John',
+            'email_address': 'john@example.com',
+            'support_type': support_type
+        }
     )
 
-    assert normalize_spaces(page.select_one('h1').text) == 'Thanks for contacting us'
+    page = client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={'message': ''}
+    )
+
+    assert_has_back_link(page)
+
+    assert [
+        (error['data-error-label'], normalize_spaces(error.text))
+        for error in page.select('.error-message')
+    ] == [
+        ('message', 'This field is required.')
+    ]
+
+    mock_send_contact_email.assert_not_called()
+
+
+def test_saves_form_to_session(client_request, mocker):
+    mock_send_contact_email = mocker.patch('app.user_api_client.send_contact_email')
+
+    client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={
+            'name': '',
+            'email_address': 'john@example.com',
+            'support_type': 'other'
+        }
+    )
+
+    # Leaving form
+    client_request.get('.security')
+
+    # Going back
+    page = client_request.get('.contact', _test_page_title=False)
+
+    # Fields have been saved and are filled
+    assert page.select_one('input[checked]')['value'] == 'other'
+    assert [
+        (input['name'], input['value'])
+        for input in page.select('input')
+        if input['name'] in ['name', 'email_address']
+    ] == [
+        ('name', ''),
+        ('email_address', 'john@example.com')
+    ]
+
+    # Validating first step
+    client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={
+            'name': 'John',
+            'email_address': 'john@example.com',
+            'support_type': 'other'
+        }
+    )
+
+    # Leaving form
+    client_request.get('.security')
+
+    # Going back
+    page = client_request.get('.contact', _test_page_title=False)
+    assert normalize_spaces(page.find('h1').text) == 'Tell us more'
+
+    # Filling a message
+    page = client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={'message': 'My message'}
+    )
+
+    assert_no_back_link(page)
+    assert normalize_spaces(page.find('h1').text) == 'Thanks for contacting us'
+
+    mock_send_contact_email.assert_called_once()
+
+    # Going back
+    page = client_request.get('.contact', _test_page_title=False)
+
+    # Fields are blank
+    assert page.select_one('input[checked]') is None
+    assert [
+        (input['name'], input['value'])
+        for input in page.select('input')
+        if input['name'] in ['name', 'email_address']
+    ] == [
+        ('name', ''),
+        ('email_address', '')
+    ]
+
+
+@pytest.mark.parametrize('support_type, expected_heading, friendly_support_type', [
+    ('ask_question', 'Ask a question', 'Ask a question'),
+    ('technical_support', 'Get technical support', 'Get technical support'),
+    ('give_feedback', 'Give feedback', 'Give feedback'),
+    ('other', 'Tell us more', 'Other'),
+])
+def test_all_reasons_message_step_success(
+    client_request,
+    mocker,
+    support_type,
+    expected_heading,
+    friendly_support_type,
+):
+
+    mock_send_contact_email = mocker.patch('app.user_api_client.send_contact_email')
+
+    page = client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={
+            'name': 'John',
+            'support_type': support_type,
+            'email_address': 'john@example.com',
+        }
+    )
+
+    assert_has_back_link(page)
+    assert normalize_spaces(page.find('h1').text) == expected_heading
+
+    message = 'This is my message'
+    page = client_request.post(
+        '.contact',
+        _expected_status=200,
+        _data={'message': message}
+    )
+
+    assert_no_back_link(page)
+    assert normalize_spaces(page.find('h1').text) == 'Thanks for contacting us'
+
+    profile = url_for('.user_information', user_id=current_user.id, _external=True)
+    expected_message = f'{message}<br><br>---<br><br> {profile}'
+
+    mock_send_contact_email.assert_called_once_with(
+        'John',
+        'john@example.com',
+        expected_message,
+        friendly_support_type
+    )
+
+
+def test_demo_steps_success(client_request, mocker):
+    mock_send_contact_email = mocker.patch('app.user_api_client.send_contact_email')
+
+    data = {
+        'name': 'John',
+        'email_address': 'john@example.com',
+        'support_type': 'demo',
+        'department_org_name': 'My department',
+        'program_service_name': 'My service',
+        'intended_recipients': 'public',
+        'main_use_case': 'status_updates',
+        'main_use_case_details': 'Password resets for our app',
+    }
+
+    def submit_form(keys):
+        return client_request.post(
+            '.contact',
+            _expected_status=200,
+            _data={k: v for k, v in data.items() if k in keys}
+        )
+
+    # Identity step
+    page = submit_form(['name', 'email_address', 'support_type'])
+
+    # Department step
+
+    assert_has_back_link(page)
+    assert normalize_spaces(page.find('h1').text) == 'Set up a demo'
+    assert 'Step 1 of 2' in page.text
+    page = submit_form(['department_org_name', 'program_service_name', 'intended_recipients'])
+
+    # Main use case step
+    assert len(page.select('.back-link')) == 1
+    assert normalize_spaces(page.find('h1').text) == 'Set up a demo'
+    assert 'Step 2 of 2' in page.text
+    page = submit_form(['main_use_case', 'main_use_case_details'])
+
+    # Thank you page
+    assert_no_back_link(page)
+    assert normalize_spaces(page.find('h1').text) == 'Thanks for contacting us'
+
+    expected_message = '<br><br>'.join([
+        f'- user: {data["name"]} {data["email_address"]}',
+        f'- department/org: {data["department_org_name"]}',
+        f'- program/service: {data["program_service_name"]}',
+        f'- intended_recipients: {data["intended_recipients"]}',
+        f'- main use case: {data["main_use_case"]}',
+        f'- main use case details: {data["main_use_case_details"]}',
+        '---',
+        f" {url_for('.user_information', user_id=current_user.id, _external=True)}"
+    ])
+
     mock_send_contact_email.assert_called_once_with(
         data['name'],
         data['email_address'],
-        data['feedback'],
-        data['support_type']
+        expected_message,
+        'Set up a demo to learn more about GC Notify'
     )
+
+    # Going back
+    page = client_request.get('.contact', _test_page_title=False)
+
+    # Fields are blank
+    assert page.select_one('input[checked]') is None
+    assert [
+        (input['name'], input['value'])
+        for input in page.select('input')
+        if input['name'] in ['name', 'email_address']
+    ] == [
+        ('name', ''),
+        ('email_address', '')
+    ]
+
+
+@pytest.mark.parametrize('input_name, input_value, has_error', [
+    ('name', '', True),
+    ('email_address', 'invalid', True),
+    ('support_type', 'invalid', True),
+    ('department_org_name', '', True),
+    ('program_service_name', '', True),
+    ('intended_recipients', 'invalid', True),
+    ('main_use_case', 'invalid', True),
+    ('main_use_case_details', '', True),
+    ('main_use_case_details', 'awesome', False),
+])
+def test_demo_steps_validation(
+    client_request,
+    mocker,
+    input_name,
+    input_value,
+    has_error,
+):
+    mock_send_contact_email = mocker.patch('app.user_api_client.send_contact_email')
+
+    valid_data = {
+        'name': 'John',
+        'email_address': 'john@example.com',
+        'support_type': 'demo',
+        'department_org_name': 'My department',
+        'program_service_name': 'My service',
+        'intended_recipients': 'public',
+        'main_use_case': 'status_updates',
+        'main_use_case_details': 'Password resets for our app',
+    }
+
+    def submit_form(keys):
+        keys_to_use = [k for k in keys if k != input_name]
+        default_data = {k: v for k, v in valid_data.items() if k in keys_to_use}
+        return client_request.post(
+            '.contact',
+            _expected_status=200,
+            _data={**default_data, **{input_name: input_value}}
+        )
+
+    fields_by_step = [
+        ['name', 'email_address', 'support_type'],
+        ['department_org_name', 'program_service_name', 'intended_recipients'],
+        ['main_use_case', 'main_use_case_details'],
+    ]
+
+    for fields_group in fields_by_step:
+        # Submit as many steps as we can until we encounter an error
+        # caused by a custom input value
+        page = submit_form(fields_group)
+
+        # Making sure that the error is flagged
+        if input_name in fields_group and has_error:
+            assert [
+                error['data-error-label'] for error in page.select('.error-message')
+            ] == [input_name]
+            mock_send_contact_email.assert_not_called()
+            return
+        # Submitted only valid data, no errors
+        else:
+            assert page.select('.error-message') == []
+
+    mock_send_contact_email.assert_called_once()
