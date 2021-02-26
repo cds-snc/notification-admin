11049849d067f4794f132e742b066c622a71cc17
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-02-26 15:28:16 -0500

    feat: generate EN translations from FR file

---
+++ scripts/generate_en_translations.py
@@ -0,0 +1,19 @@
+import csv
+from pathlib import Path
+
+root_folder = Path(__file__).parent.parent.absolute()
+translation_folder = "app/translations/csv"
+
+fr_file = root_folder / translation_folder / "fr.csv"
+en_file = root_folder / translation_folder / "en.csv"
+
+headers = ["source", "target"]
+
+with open(fr_file) as fr, open(en_file, "w") as en:
+    reader = csv.DictReader(fr, fieldnames=headers)
+    writer = csv.DictWriter(en, fieldnames=headers, quoting=csv.QUOTE_ALL)
+
+    for row in reader:
+        writer.writerow({"source": row["source"], "target": ""})
+
+    print(f"Generated {en_file} from {fr_file}")
