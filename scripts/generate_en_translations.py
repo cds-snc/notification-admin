cd75d48e0746dac67d9ffd44089f5c1aa3711629
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-03-01 09:45:16 -0500

    Add en_base.csv file

--- scripts/generate_en_translations.py
+++ scripts/generate_en_translations.py
@@ -5,13 +5,23 @@ root_folder = Path(__file__).parent.parent.absolute()
 translation_folder = "app/translations/csv"
 
 fr_file = root_folder / translation_folder / "fr.csv"
+en_base_file = root_folder / translation_folder / "en_base.csv"
 en_file = root_folder / translation_folder / "en.csv"
 
 headers = ["source", "target"]
 
-with open(fr_file) as fr, open(en_file, "w") as en:
+with open(fr_file) as fr, open(en_base_file) as en_base, open(en_file, "w") as en:
     reader = csv.DictReader(fr, fieldnames=headers)
-    writer = csv.DictWriter(en, fieldnames=headers, quoting=csv.QUOTE_ALL)
+    base_en_reader = csv.DictReader(en_base, fieldnames=headers)
 
+    # Write a few English overrides
+    en_writer = csv.DictWriter(en, fieldnames=headers, quoting=csv.QUOTE_ALL)
+    translated = set()
+    for row in base_en_reader:
+        en_writer.writerow({"source": row["source"], "target": row["target"]})
+        translated.add(row["source"])
+
+    # Fill up the en.csv file with empty strings based on the fr.csv file
     for row in reader:
-        writer.writerow({"source": row["source"], "target": ""})
+        if row["source"] not in translated:
+            en_writer.writerow({"source": row["source"], "target": ""})
