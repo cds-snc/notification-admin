80bf13d8cdb1a27a43a0cd85299a773968398826
Stephen McMurtry <smcmurtry@gmail.com>
2021-06-07 11:12:04 -0600

    ignored some type errors

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,8 +17,8 @@ class dotdict(dict):
     """dot.notation access to dictionary attributes"""
 
     __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__  # type: ignore
+    __delattr__ = dict.__delitem__  # type: ignore
 
 
 class TestClient(FlaskClient):
