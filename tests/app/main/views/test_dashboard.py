80f409c8866f7ea39b006b7b9f300d57fc5e43f1
Stephen McMurtry <smcmurtry@gmail.com>
2021-06-10 13:23:14 -0600

    added another test

--- tests/app/main/views/test_dashboard.py
+++ tests/app/main/views/test_dashboard.py
@@ -202,6 +202,31 @@ def test_task_shortcuts_are_visible_based_on_permissions(
         assert text not in page.text
 
 
+def test_sending_link_has_query_param(
+    client_request: ClientRequest,
+    active_user_with_permissions,
+    mocker,
+    mock_get_service_templates,
+    mock_get_jobs,
+    mock_get_service_statistics,
+    mock_get_usage,
+    mock_get_inbound_sms_summary,
+):
+    mocker.patch(
+        "app.template_statistics_client.get_template_statistics_for_service",
+        return_value=copy.deepcopy(stub_template_stats),
+    )
+    active_user_with_permissions["permissions"][SERVICE_ONE_ID] = ["view_activity", "send_messages"]
+    client_request.login(active_user_with_permissions)
+
+    page = client_request.get(
+        "main.service_dashboard",
+        service_id=SERVICE_ONE_ID,
+    )
+    sending_url = url_for("main.choose_template", service_id=SERVICE_ONE_ID, view="sending")
+    assert sending_url == page.select_one("a.button").attrs["href"]
+
+
 def test_should_show_recent_templates_on_dashboard(
     client_request,
     mocker,
