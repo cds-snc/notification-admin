d11fc01ebc6d8c0641c788326825754814aaaa49
iokpala <ikenna.okpala@cds-snc.ca>
2021-10-14 17:43:41 -0300

    Revert "Merge branch 'master' into hot-fix/text-should-be-marked-as-legend"
    This reverts commit 05a6f93be33e128d6f5ac25b8ecd70662ee57f8f, reversing
    changes made to d585c3c9d0208b027cf375df2e248e800d0e2b98.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   theme: {
@@ -15,7 +15,6 @@ module.exports = {
       outset2neg: "0 -2px 0 0 rgba(191, 193, 195, 0.2)",
       outset3: "3px 0 0 0 rgba(191, 193, 195, 0.2)",
       yellow3: "-3px 0 0 0 rgba(255, 191, 71, 1), 3px 0 0 0 rgba(255, 191, 71, 1)",
-      focusLine2: "0 2px 0 3px #1A3152"
     },
     fontSize: {
       xs: "1.3rem",
@@ -54,7 +53,7 @@ module.exports = {
         border: "#1A3152",
         selected: "#75b9e0",
         lightblue25: "#d5e8f3",
-        lightblue: "#0154B0",
+        lightblue: "#2b8cc4",
         slightlight: "#284162"
       },
       gray: {
@@ -68,7 +67,7 @@ module.exports = {
         grey4: "#f8f8f8",
         lightgrey: "#C0C1C3",
         visitedlight: "#929AA4",
-        visiteddark: "#C8CDD1"
+        visiteddark: "#C8CDD1",
       },
       yellow: {
         default: "#ffbf47"
@@ -78,18 +77,18 @@ module.exports = {
         darker: "#00703C",
         hover: "#00692f",
         border: "#003618",
-        green: "#006435"
+        green: "#006435",
       },
       black: {
         default: "#000"
       },
       transparent: {
         default: "transparent"
-      },
+      }
     },
     extend: {
       animation: {
-        ellipsis: "ellipsis steps(4,end) 1.5s infinite"
+        'ellipsis': 'ellipsis steps(4,end) 1.5s infinite',
       },
       backgroundImage: {
         tick: "url('/static/images/tick.png')",
@@ -100,60 +99,60 @@ module.exports = {
         folderBlue: "url('/static/images/folder-blue-bold.svg')",
         folderBluePng: "url('/static/images/folder-blue-bold.png')",
         folderBlueHover: "url('/static/images/folder-blue-bold-hover.svg')",
-        folderBlueHoverPng: "url('/static/images/folder-blue-bold-hover.png')"
+        folderBlueHoverPng: "url('/static/images/folder-blue-bold-hover.png')",
       },
       backgroundSize: {
-        '19': "19px"
+        '19': '19px',
       },
       borderWidth: {
-        '1': "1px",
-        '10': "10px"
+        '1': '1px',
+        '10': '10px',
       },
       fontFamily: {
         sans: ["lato"],
         body: ["Noto Sans", "Arial", "sans-serif"],
-        monospace: ["monospace"]
+        monospace: ["monospace"],
       },
       inset: {
-        '2': "2px",
-        '5': "5px",
-        '7': "7px"
+        '2': '2px',
+        '5': '5px',
+        '7': '7px',
       },
       keyframes: {
-        ellipsis: {
-          '100%': { width: "1.25em" }
-        },
+        'ellipsis': {
+          '100%': { width: '1.25em' }
+        }
       },
       lineHeight: {
-        'extra-tight': "0.9"
+        'extra-tight': '0.9',
       },
       maxWidth: {
-        '4xl': "53rem"
+        "4xl": "53rem"
       },
       outline: {
-        yellow: "3px solid #ffbf47",
-        white: "1px solid rgba(255, 255, 255, 0.1)"
+        yellow: '3px solid #ffbf47',
+        white: '1px solid rgba(255, 255, 255, 0.1)',
       },
       spacing: {
-        gutter: "30px",
-        gutterHalf: "15px",
-        gutterAndAHalf: "45px",
-        doubleGutter: "60px"
+        gutter: '30px',
+        gutterHalf: '15px',
+        gutterAndAHalf: '45px',
+        doubleGutter: '60px',
       },
       transitionDuration: {
-        '600': "600ms"
+        '600': '600ms',
       },
       transitionProperty: {
-        background: "background"
+        'background': 'background',
       },
       width: {
-        '5/8': "62.5%"
+        '5/8': '62.5%'
       },
       flex: {
         2: "2 2 0%",
       },
       zIndex: {
-        '100': 100
+        '100': 100,
       },
     },
   },
@@ -161,28 +160,28 @@ module.exports = {
     plugin(function ({ addVariant, e }) {
       addVariant('link', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `.${e(`link${separator}${className}`)}:link`;
-        });
-      });
+          return `.${e(`link${separator}${className}`)}:link`
+        })
+      })
     }),
     plugin(function ({ addUtilities, theme }) {
       const individualBorderColors = {
-        ".border-b-gray-button": {
-          borderBottomColor: theme("colors").gray.button,
+        '.border-b-gray-button': {
+          borderBottomColor: theme('colors').gray.button
         },
-        ".border-l-gray-button": {
-          borderLeftColor: theme("colors").gray.button,
+        '.border-l-gray-button': {
+          borderLeftColor: theme('colors').gray.button
         },
-        ".border-b-gray-grey2": {
-          borderBottomColor: theme("colors").gray.grey2,
-        },
+        '.border-b-gray-grey2': {
+          borderBottomColor: theme('colors').gray.grey2
+        }
       };
 
       addUtilities(individualBorderColors);
     }),
   ],
   variants: {
-    borderWidth: ["responsive", "focus"],
-    textColor: ["visited", "link", "hover", "focus"],
+    borderWidth: ['responsive', 'focus'],
+    textColor: ['visited', 'link', 'hover', 'focus'],
   },
 };
