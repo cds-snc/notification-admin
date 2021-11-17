81654b13337aac555de0e42d14cbae7f17b6b933
Philippe Caron <amazingphilippe@gmail.com>
2021-11-17 13:52:18 -0500

    working keyboard implementation. Untested

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,52 +3,146 @@
 
   const registerKeyDownEscape = window.utils.registerKeyDownEscape;
 
+  function open($menu, $items) {
+    // show menu
+    $items.toggleClass("hidden", false);
+    $items.removeClass("opacity-0");
+    $items.addClass("opacity-100");
+    setFocusToFirstItem($items);
+    const $arrow = $menu.find(".arrow");
+    if ($arrow.length > 0) {
+      $arrow.toggleClass("flip", true);
+    }
+    $menu.attr("aria-expanded", true);
+    console.log("open");
+  }
+
+  function close($menu, $items) {
+    // hide menu
+    $items.toggleClass("hidden", true);
+    $items.removeClass("opacity-100");
+    $items.addClass("opacity-0");
+    const $arrow = $menu.find(".arrow");
+    if ($arrow.length > 0) {
+      $arrow.toggleClass("flip", false);
+    }
+    $menu.attr("aria-expanded", false);
+    console.log("close");
+  }
+
   function toggleMenu($menu, $items) {
-    // We need to revert the hidden and opacity operations when showing and
-    // hiding the menu.
+    const show = $items.hasClass("hidden");
 
     // Show the menu..
-    const show = $items.hasClass("hidden");
-    $menu.attr("aria-expanded", show);
     if (show) {
-      $items.toggleClass("hidden", false);
+      open($menu, $items);
     }
     // Hide the menu..
     else {
-      $items.removeClass("opacity-100");
-      $items.addClass("opacity-0");
+      close($menu, $items);
     }
+  }
 
-    const $arrow = $menu.find(".arrow");
-    if ($arrow.length > 0) {
-      $arrow.toggleClass("flip", show);
-    }
+  function setFocusToLastItem($items) {
+    const $menuItems = $items.children("[role='menuitem']");
+    $menuItems.last().focus().attr("tabindex", 0);
+  }
+  function setFocusToFirstItem($items) {
+    const $menuItems = $items.children("[role='menuitem']");
+    $menuItems.first().focus().attr("tabindex", 0);
+  }
 
-    // In order to have the opacity transition effect working properly,
-    // we need to separate the hidden and opacity toggling in two separate
-    // actions in the HTML rendering, hence the delay of opacity by 1 ms.
-    window.setTimeout(function () {
-      if (show) {
-        // show menu
-        $items.removeClass("opacity-0");
-        $items.addClass("opacity-100");
-      } else {
-        // hide menu
-        $items.toggleClass("hidden", true);
-      }
-    }, 1);
+  function setFocusToNextItem($items, $current) {
+    const $menuItems = $items.children("[role='menuitem']");
+    const i = $current.index();
+    $menuItems
+      .eq(i + 1 === $menuItems.length ? 0 : i + 1)
+      .focus()
+      .attr("tabindex", 0);
+  }
+  function setFocusToPreviousItem($items, $current) {
+    const $menuItems = $items.children("[role='menuitem']");
+    const i = $current.index();
+    $menuItems
+      .eq(i - 1 === -1 ? $menuItems.length - 1 : i - 1)
+      .focus()
+      .attr("tabindex", 0);
   }
 
   function init($menu) {
     const itemsId = "#" + $menu.attr("data-menu-items");
     const $items = $(itemsId);
+    const $menuItems = $items.children("[role='menuitem']");
 
-    const fn = () => toggleMenu($menu, $items);
-    $menu.click(fn);
-    registerKeyDownEscape($items, fn);
+    $menu.click(() => toggleMenu($menu, $items));
+    registerKeyDownEscape($items, () => close($menu, $items));
+    // Key handlers for menubutton
+    $menu.keydown((e) => {
+      let flag = false;
+      console.log(e.code, e.target.text());
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+        case "ArrowDown":
+          if ($menu) {
+            open($menu, $items);
+            flag = true;
+          }
+          break;
+        case "ArrowUp":
+          if ($menu) {
+            open($menu, $items);
+            setFocusToLastItem($items);
+            flag = true;
+          }
+          break;
+        case "Escape":
+        case "Tab":
+          close($menu, $items);
+          break;
+        default:
+          break;
+      }
+      if (flag) {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("flag", flag);
+      }
+    });
+
+    //Key handlers for menu items
+    $menuItems.keydown((e) => {
+      let flag = false;
+      console.log(e.code, e.target.text());
+      switch (e.code) {
+        case "ArrowDown":
+          setFocusToNextItem($items, $(e.target));
+          flag = true;
+          break;
+        case "ArrowUp":
+          setFocusToPreviousItem($items, $(e.target));
+          flag = true;
+          break;
+        case "Escape":
+        case "Tab":
+          close($menu, $items);
+          break;
+        default:
+          break;
+      }
+      if (flag) {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("flag", flag);
+      }
+    });
   }
 
   Modules.Menu = function () {
+    this.menuItems = [];
+    this.firstItem = null;
+    this.lastItem = null;
+
     this.start = function (component) {
       let $component = $(component);
       init($component);
--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -8,7 +8,7 @@
    * @param {*} fn Function to execute on Escape key press
    */
   function registerKeyDownEscape($selector, fn) {
-    $selector.keydown(function (e) {
+    $(window).keydown(function (e) {
       if (e.key == "Escape") {
         const displayed = !!$selector.not(":hidden");
         if (displayed) fn();
