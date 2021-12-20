0336e7f4b7d17abe815ad1bc44b164651ca8f325
Stephen Astels <sastels@gmail.com>
2021-12-20 16:25:37 -0500

    fix tests

--- tests/app/notify_client/test_notify_admin_api_client.py
+++ tests/app/notify_client/test_notify_admin_api_client.py
@@ -21,6 +21,7 @@ from tests.conftest import api_user_active, platform_admin_user, set_config
 @pytest.mark.parametrize("service", [service_json(active=True), None], ids=["active_service", "no_service"])
 def test_active_service_can_be_modified(app_, method, user, service):
     api_client = NotifyAdminAPIClient()
+    api_client.init_app(app_)
 
     with app_.test_request_context() as request_context, app_.test_client() as client:
         client.login(user)
@@ -36,6 +37,7 @@ def test_active_service_can_be_modified(app_, method, user, service):
 @pytest.mark.parametrize("method", ["put", "post", "delete"])
 def test_inactive_service_cannot_be_modified_by_normal_user(app_, api_user_active, method):
     api_client = NotifyAdminAPIClient()
+    api_client.init_app(app_)
 
     with app_.test_request_context() as request_context, app_.test_client() as client:
         client.login(api_user_active)
@@ -51,6 +53,7 @@ def test_inactive_service_cannot_be_modified_by_normal_user(app_, api_user_activ
 @pytest.mark.parametrize("method", ["put", "post", "delete"])
 def test_inactive_service_can_be_modified_by_platform_admin(app_, platform_admin_user, method):
     api_client = NotifyAdminAPIClient()
+    api_client.init_app(app_)
 
     with app_.test_request_context() as request_context, app_.test_client() as client:
         client.login(platform_admin_user)
@@ -118,6 +121,7 @@ def test_get_notification_status_by_service(mocker):
 @pytest.mark.parametrize("method", ["put", "post", "delete"])
 def test_logging_enabled_for_admin_users(app_, platform_admin_user, method, caplog):
     api_client = NotifyAdminAPIClient()
+    api_client.init_app(app_)
 
     with app_.test_request_context() as request_context, app_.test_client() as client:
         client.login(platform_admin_user)
