61d8987f72e02baa3b6e2870745704954a0b0e2d
Stephen Astels <sastels@gmail.com>
2021-12-20 17:08:34 -0500

    add test

--- app/notify_client/__init__.py
+++ app/notify_client/__init__.py
@@ -23,7 +23,7 @@ class NotifyAdminAPIClient(BaseAPIClient):
         self.service_id = app.config["ADMIN_CLIENT_USER_NAME"]
         self.api_key = app.config["ADMIN_CLIENT_SECRET"]
         self.route_secret = app.config["ROUTE_SECRET_KEY_1"]
-        self.sensitive_services = app.config["SENSITIVE_SERVICES"].split(",")
+        self.sensitive_services = [x.strip() for x in app.config["SENSITIVE_SERVICES"].split(",")]
 
     def generate_headers(self, api_token):
         headers = {
@@ -53,7 +53,11 @@ class NotifyAdminAPIClient(BaseAPIClient):
             abort(403)
 
     def log_admin_call(self, url, method):
-        service_is_sensitive = any(service in url for service in self.sensitive_services)
+        # this file is imported in app/__init__.py before current_service is initialised, so need to import later
+        # to prevent cyclical imports
+        from app import current_service
+
+        service_is_sensitive = current_service and any(service == current_service.id for service in self.sensitive_services)
         if hasattr(current_user, "platform_admin") and current_user.platform_admin:
             user = current_user.email_address + "|" + current_user.id
             logger.warn("{}Admin API request {} {} {} ".format("Sensitive " if service_is_sensitive else "", method, url, user))
