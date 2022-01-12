4296f3100d11216037dfde6bf89e6e7cdf9e2dfc
iokpala <ikenna.okpala@cds-snc.ca>
2022-01-12 12:23:07 -0400

    WIP

--- config/mixpanel.py
+++ config/mixpanel.py
@@ -1,4 +1,5 @@
 import os
+from pdb import set_trace
 import flask
 
 from app.models.user import User
@@ -6,12 +7,13 @@ from mixpanel import Mixpanel  # type: ignore
 
 
 class NotifyMixpanel:
-    def _check_mixpanel() -> bool:
+    def __new__(klazz):
         if not os.environ.get("MIXPANEL_PROJECT_TOKEN"):
             flask.current_app.logger.warning(
                 "MIXPANEL_PROJECT_TOKEN is not set. Mixpanel features will not be supported."
                 "In order to enable Mixpanel, set MIXPANEL_PROJECT_TOKEN environment variable."
             )
+            
             return False
         else:
             return True
@@ -20,17 +22,29 @@ class NotifyMixpanel:
         super().__init__()
 
         self.mixpanel: Mixpanel = Mixpanel(os.environ.get("MIXPANEL_PROJECT_TOKEN", ""))
-        self._enabled = self._check_mixpanel()
 
+
+    def __mixpanel_enabled(callable):
+        def wrapper(*args, **kwargs):
+            outcome = _check_mixpanel()
+
+            import pdb; pdb.set_trace()
+
+            if outcome:
+                callable(*args, **kwargs)
+                
+        
+        return wrapper
+
+    @__mixpanel_enabled
     def track_event(self, user: User, msg="Notify: Sent message") -> None:
-        if self._enabled:
-            self.mixpanel.track(user.email_address, msg, {"product": "Notify"})
+        self.mixpanel.track(user.email_address, msg, {"product": "Notify"})
 
+    @__mixpanel_enabled
     def track_user_profile(self, user: User) -> None:
-        if self._enabled:
-            profile = {
-                "$first_name": user.name.split()[0],
-                "$last_name": user.name.split()[-1],
-                "$email": user.email_address,
-            }
-            self.mixpanel.people_set(user.email_address, profile, meta={"product": "Notify"})
+        profile = {
+            "$first_name": user.name.split()[0],
+            "$last_name": user.name.split()[-1],
+            "$email": user.email_address,
+        }
+        self.mixpanel.people_set(user.email_address, profile, meta={"product": "Notify"})
