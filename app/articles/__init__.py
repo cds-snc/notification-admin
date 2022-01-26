4b380d49e2cfd921a2715848c4f57f88da602903
Paul Craig <paul@pcraig3.ca>
2022-01-26 11:22:00 -0500

    Refactor getting a preview URL, remove "g.preview"
    We use the preview URL for the "Edit this page" link in the preview
    banner.
    
    Previously, I was setting 2 variables: one was a boolean set to True if
    we were in preview mode, and the second was the preview URL.
    
    It occurred to me that we would never have a preview URL if preview was
    false, so we can just check for the existence of a preview URL as a
    truthy value and delete the other param.
    
    The other change here was to move the URL generation into its own function
    in "app.articles". It needs to know what the current language is so that
    WordPress can generate the right Preview link, and all the other functions
    in app.articles are getting the current language themselves rather than
    having the view do anything with it.
    
    Refactor the get preview URL method

--- app/articles/__init__.py
+++ app/articles/__init__.py
@@ -153,3 +153,11 @@ def get_lang_url(response, page_id):
 
     lang_slug = response.get(f"slug_{alt_lang}") or response.get("slug")
     return f"/{lang_slug}"
+
+
+# Return URL for the "Edit this page" link in the preview banner header
+def get_preview_url(page_id):
+    lang = get_current_locale(current_app)
+    base_endpoint = current_app.config["GC_ARTICLES_API"]
+
+    return f"https://{base_endpoint}/wp-admin/post.php?post={page_id}&action=edit&lang={lang}"
