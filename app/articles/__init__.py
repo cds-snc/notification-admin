757545a16148fe2899b44d434caf75eb3405e23a
Paul Craig <paul_craig_16@hotmail.com>
2022-02-01 16:32:42 -0500

    Language switcher for pages being returned from the API (#1225)
    * Add translated API pages to language switcher
    
    Added an extra variable to the "page-content" template, and defaulting
    to the old URL if our new one isn't passed in.
    
    * Refactor getting a preview URL, remove "g.preview"
    
    We use the preview URL for the "Edit this page" link in the preview
    banner.
    
    Previously, I was setting 2 variables: one was a boolean set to True if
    we were in preview mode, and the second was the preview URL.
    
    It occurred to me that we would never have a preview URL if preview was
    false, so we can just check for the existence of a preview URL as a
    truthy value and delete the other param.
    
    The other change here was to move the URL generation into its own function
    in "app.articles". It needs to know what the current language is so that
    WordPress can generate the right Preview link, and all the other functions
    in app.articles are getting the current language themselves rather than
    having the view do anything with it.
    
    Refactor the get preview URL method
    
    * Add typehinting and tests for get_preview_url
    
    Always return a URL string, even in a bad page_id is passed in.
    
    This is because we catch the page_id before we get here.
    It's also because we don't want to 'abort' inside of app.articles
    
    * Add typehinting and tests for get_lang_url
    
    * Explicitly 404 if there is no translated page_id
    
    If there is no translated page available, get_lang_url would default
    to returning the current page slug or the current ID.
    
    If it returns the current page slug, WordPress will send back a 404
    response (because it doesn't see that slug for the other language),
    but the ID would just return as normal.
    
    Speaking to Elise, we preferred to clearly indicate something is
    wrong rather than make it look like a page exists but wasn't translated yet.
    
    * Remove preview boarder, add z-index
    
    Removing the border to cut some of the visual noise.
    Adding a z-index so that it will be on top of full-screen elements.
    
    * Add '.wp' body class to API-driven pages

--- app/articles/__init__.py
+++ app/articles/__init__.py
@@ -137,3 +137,32 @@ def _get_nav_wp(locale: str) -> Optional[list]:
         nav_items.append({"title": "preview", "url": "/preview", "target": "", "description": ""})
 
     return nav_items
+
+
+# Return URL path for the language switcher
+def get_lang_url(response: dict, has_page_id: bool) -> str:
+    # url either looks like:
+    # - /preview?id=11
+    # - /wild-card
+
+    alt_lang = "fr" if (get_current_locale(current_app) == "en") else "en"
+
+    if has_page_id:
+        if response.get(f"id_{alt_lang}"):
+            lang_id = response.get(f"id_{alt_lang}")
+            return f"/preview?id={lang_id}"
+        else:
+            # if no translated page id, return 404 explicitly
+            return "/404"
+
+    lang_slug = response.get(f"slug_{alt_lang}") or response.get("slug")
+    # if no translated page slug, this will 404 by itself
+    return f"/{lang_slug}"
+
+
+# Return URL for the "Edit this page" link in the preview banner header
+def get_preview_url(page_id: int) -> str:
+    lang = get_current_locale(current_app)
+    base_endpoint = current_app.config["GC_ARTICLES_API"]
+
+    return f"https://{base_endpoint}/wp-admin/post.php?post={page_id}&action=edit&lang={lang}"
