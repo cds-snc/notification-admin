9dcedc1b544e8ec9a39d61bd64314e4584156c1d
Andrew Leith <andrew.leith@cds-snc.ca>
2022-04-19 19:04:14 +0000

    fix(tests): update mocks and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1870,7 +1870,7 @@ def mock_verify_password(mocker):
 
 @pytest.fixture(scope="function")
 def mock_update_user_password(mocker, api_user_active):
-    def _update(user_id, password):
+    def _update(user_id, password, login_data={}):
         api_user_active["id"] = user_id
         return api_user_active
 
