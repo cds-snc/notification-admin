e7e4de7be38858d6d1ec6d47a3effadb45a71b60
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-27 16:16:19 -0400

    bit of refactoring

--- tests/app/articles/test_fallback_cache.py
+++ tests/app/articles/test_fallback_cache.py
@@ -12,15 +12,17 @@ from app.articles.api import get_content
 from tests import MockRedis
 
 gc_articles_api = "articles.alpha.canada.ca/notification-gc-notify"
-notify_url = f"https://{gc_articles_api}/wp-json/pages"
+notify_url = f"https://{gc_articles_api}/wp-json"
+endpoint = "wp/v2/pages"
+request_url = f"{notify_url}/{endpoint}"
 
 mock_redis_obj = MockRedis()
 mock_redis_method = MagicMock()
 mock_redis_method.get = Mock(side_effect=mock_redis_obj.get)
 mock_redis_method.set = Mock(side_effect=mock_redis_obj.set)
 
-response_json = {"content": {"rendered": "The Content"}, "title": {"rendered": "The Title"}}
-cache_key = f"{GC_ARTICLES_FALLBACK_CACHE_PREFIX}pages/en/mypage"
+response_json = json.dumps({"content": {"rendered": "The Content"}, "title": {"rendered": "The Title"}})
+cache_key = f"{GC_ARTICLES_FALLBACK_CACHE_PREFIX}{endpoint}/en/mypage"
 
 
 def test_save_to_long_term_cache(app_, mocker):
@@ -29,8 +31,8 @@ def test_save_to_long_term_cache(app_, mocker):
     with app_.test_request_context():
         mocker.patch.dict("app.current_app.config", values={"GC_ARTICLES_API": gc_articles_api})
         with requests_mock.mock() as mock:
-            mock.request("GET", notify_url, json=response_json, status_code=200)
-            response = get_content("pages", {"slug": "mypage", "lang": "en"}, cacheable=True)
+            mock.request("GET", request_url, json=response_json, status_code=200)
+            response = get_content(endpoint, {"slug": "mypage", "lang": "en"}, cacheable=True)
 
             assert mock_redis_method.set.called
             assert mock_redis_method.set.call_count == 1
@@ -44,15 +46,15 @@ def test_dont_cache_on_404(app_, mocker):
     with app_.test_request_context():
         mocker.patch.dict("app.current_app.config", values={"GC_ARTICLES_API": gc_articles_api})
         with requests_mock.mock() as mock:
-            mock.get(notify_url, json={}, status_code=404)
-            get_content("pages", {"slug": "mypage", "lang": "en"}, cacheable=True)
+            mock.get(request_url, json={}, status_code=404)
+            get_content(endpoint, {"slug": "mypage", "lang": "en"}, cacheable=True)
 
             """ Do not call cache.set """
             assert not mock_redis_method.set.called
 
 
 def test_retrieve_existing_from_fallback_cache_on_http_error(app_, mocker):
-    redis_cache = {"gc-articles-fallback--pages/en/mypage": json.dumps(response_json)}
+    redis_cache = {cache_key: json.dumps(response_json)}
 
     mock_redis_obj = MockRedis(redis_cache)
     mock_redis_method = MagicMock()
@@ -64,10 +66,10 @@ def test_retrieve_existing_from_fallback_cache_on_http_error(app_, mocker):
         mocker.patch.dict("app.current_app.config", values={"GC_ARTICLES_API": gc_articles_api})
         with requests_mock.mock() as mock:
             """ConnectionError during retrieval"""
-            mock.get(notify_url, exc=requests.exceptions.ConnectionError)
+            mock.get(request_url, exc=requests.exceptions.ConnectionError)
 
-            get_content("pages", {"slug": "mypage", "lang": "en"}, cacheable=True)
+            get_content(endpoint, {"slug": "mypage", "lang": "en"}, cacheable=True)
 
             assert mock_redis_method.get.called
-            assert mock_redis_method.get.called_with("gc-articles-fallback--pages/en/mypage")
-            assert mock_redis_method.get("gc-articles-fallback--pages/en/mypage") == json.dumps(response_json)
+            assert mock_redis_method.get.called_with(cache_key)
+            assert mock_redis_method.get(cache_key) == json.dumps(response_json)
--- tests/app/articles/test_pages.py
+++ tests/app/articles/test_pages.py
@@ -1,5 +1,6 @@
 import json
 from unittest.mock import MagicMock, Mock
+import requests
 
 import requests_mock
 
@@ -8,9 +9,10 @@ from app.articles.pages import get_page_by_slug_with_cache
 from tests import MockRedis
 
 gc_articles_api = "articles.alpha.canada.ca/notification-gc-notify"
-notify_url = f"https://{gc_articles_api}/wp-json/pages"
-
+notify_url = f"https://{gc_articles_api}/wp-json"
 endpoint = "wp/v2/pages"
+request_url = f"{notify_url}/{endpoint}"
+
 params = {"slug": "mypage", "lang": "en"}
 cache_key = f"{GC_ARTICLES_CACHE_PREFIX}{endpoint}/en/mypage"
 
@@ -29,8 +31,8 @@ def test_get_page_by_slug_with_cache_retrieve_from_cache(app_, mocker):
 
     with app_.test_request_context():
         mocker.patch.dict("app.current_app.config", values={"GC_ARTICLES_API": gc_articles_api})
-        with requests_mock.mock() as mock:
-            mock.request("GET", notify_url, json=response_json, status_code=200)
+        with requests_mock.mock() as request_mock:
+            request_mock.get(request_url, json=response_json, status_code=200)
 
             get_page_by_slug_with_cache(endpoint, params)
 
@@ -38,10 +40,11 @@ def test_get_page_by_slug_with_cache_retrieve_from_cache(app_, mocker):
             assert mock_redis_method.get.call_count == 1
             assert mock_redis_method.get.called_with(cache_key)
             assert mock_redis_method.get(cache_key) is not None
-            assert not mock.called
+
+            assert not request_mock.called
 
 
-def test_get_page_by_slug_with_cache_miss(app_, mocker):
+def test_get_page_by_slug_with_cache_miss_with_fallback(app_, mocker):
     redis_cache = {"gc-articles-fallback--pages/en/mypage": json.dumps(response_json)}
     mock_redis_obj = MockRedis(redis_cache)
     mock_redis_method = MagicMock()
@@ -53,19 +56,19 @@ def test_get_page_by_slug_with_cache_miss(app_, mocker):
     with app_.test_request_context():
         mocker.patch.dict("app.current_app.config", values={"GC_ARTICLES_API": gc_articles_api})
         with requests_mock.mock() as request_mock:
-            request_mock.get(notify_url, json=response_json, status_code=200)
+            request_mock.get(request_url, exc=requests.exceptions.ConnectionError)
+
+            assert mock_redis_method.get(cache_key) is None
 
             get_page_by_slug_with_cache(endpoint, params)
 
-            assert mock_redis_method.get.called
-            assert mock_redis_method.get.call_count == 1
-            assert mock_redis_method.get.called_with(cache_key)
-            assert mock_redis_method.get(cache_key) is None
+            assert request_mock.called
 
+            assert mock_redis_method.get.called
+            assert mock_redis_method.get.call_count == 2
+            assert mock_redis_method.get.called_with(cache_key)
+            
             """ Should fall through to the fallback cache """
             assert mock_redis_method.get.called_with("gc-articles-fallback--pages/en/mypage")
             assert mock_redis_method.get("gc-articles-fallback--pages/en/mypage") is not None
             assert mock_redis_method.get("gc-articles-fallback--pages/en/mypage") == json.dumps(response_json)
-
-            assert request_mock.called
-            # assert mock_redis_method.set.called
