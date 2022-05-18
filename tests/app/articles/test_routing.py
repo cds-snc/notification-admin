fc85c2ff67d12c15cb1a29c289be6e1393f3d05b
Andrew Leith <andrew.leith@cds-snc.ca>
2022-05-18 12:12:40 +0000

    tests(GCA): ensure routes in GC_ARTICLES_ROUTES are valid in both languages

--- tests/app/articles/test_routing.py
+++ tests/app/articles/test_routing.py
@@ -1,9 +1,10 @@
 import pytest
+from flask import session
 
-from app.articles.routing import gca_url_for
+from app.articles.routing import GC_ARTICLES_ROUTES, gca_url_for
 
 
-@pytest.mark.parametrize("route, lang, expectedURL", [("home", "en", "/home"), ("home", "fr", "/acceuil")])
+@pytest.mark.parametrize("route, lang, expectedURL", [("home", "en", "/home"), ("home", "fr", "/accueil")])
 def test_gca_url_for_works_with_valid_routes(mocker, route, lang, expectedURL):
     mocker.patch("app.articles.routing.get_current_locale", return_value=lang)
 
@@ -19,7 +20,7 @@ def test_gca_url_for_fails_with_invalid_routes(mocker, route, lang):
 
 
 @pytest.mark.parametrize(
-    "route, lang, expectedURL", [("home", "en", "http://localhost/home"), ("home", "fr", "http://localhost/acceuil")]
+    "route, lang, expectedURL", [("home", "en", "http://localhost/home"), ("home", "fr", "http://localhost/accueil")]
 )
 def test_gca_url_for_creates_asbolute_url(app_, mocker, route, lang, expectedURL):
     mocker.patch("app.articles.routing.get_current_locale", return_value=lang)
@@ -27,3 +28,29 @@ def test_gca_url_for_creates_asbolute_url(app_, mocker, route, lang, expectedURL
     route = gca_url_for(route, _external=True)
 
     assert route == expectedURL
+
+
+@pytest.mark.parametrize("route", list(GC_ARTICLES_ROUTES))
+def test_ensure_all_french_gca_routes_in_GC_ARTICLES_ROUTES_exist(client_request, mocker, route):
+
+    mocker.patch("app.articles.routing.get_current_locale", return_value="fr")
+    mocker.patch("app.main.views.index.get_current_locale", return_value="fr")
+    render_article = mocker.patch("app.main.views.index._render_articles_page", return_value="")
+
+    url = gca_url_for(route)
+    page = client_request.get_url(url, _expected_status=200, _test_page_title=False)
+
+    assert render_article.called
+
+
+@pytest.mark.parametrize("route", list(GC_ARTICLES_ROUTES))
+def test_ensure_all_english_gca_routes_in_GC_ARTICLES_ROUTES_exist(client_request, mocker, route):
+
+    mocker.patch("app.articles.routing.get_current_locale", return_value="en")
+    mocker.patch("app.main.views.index.get_current_locale", return_value="en")
+    render_article = mocker.patch("app.main.views.index._render_articles_page", return_value="")
+
+    url = gca_url_for(route)
+    page = client_request.get_url(url, _expected_status=200, _test_page_title=False)
+
+    assert render_article.called
