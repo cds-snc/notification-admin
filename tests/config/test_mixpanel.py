8db46c24e3c54fe7834f4b1f8b40cfb4b6ce692a
Andrew Leith <andrew.leith@cds-snc.ca>
2022-06-22 12:05:45 +0000

    tests(test_mixpanel): pytest upgrade

--- tests/config/test_mixpanel.py
+++ tests/config/test_mixpanel.py
@@ -8,13 +8,14 @@ from tests.conftest import active_user_with_permissions, fake_uuid
 
 
 @pytest.fixture(autouse=True)
-def environment_vars_fixtures():
-    environment_vars = os.environ.copy()
-    os.environ["MIXPANEL_PROJECT_TOKEN"] = "project_token_from_mixpanel"
+def environment_vars_fixtures(app_):
+    with app_.app_context():
+        environment_vars = os.environ.copy()
+        os.environ["MIXPANEL_PROJECT_TOKEN"] = "project_token_from_mixpanel"
 
-    yield
+        yield
 
-    os.environ = environment_vars
+        os.environ = environment_vars
 
 
 def test_when_mixpanel_project_token_is_not_set(mocker, environment_vars_fixtures):
@@ -39,9 +40,9 @@ def test_track_mixpanel_user_profile_when_user_is_not_present(mocker, environmen
     mocked_mixpanel_people_set_fxn.assert_not_called()
 
 
-def test_track_mixpanel_user_profile(mocker, environment_vars_fixtures):
+def test_track_mixpanel_user_profile(mocker, active_user_with_permissions, environment_vars_fixtures):
     mocked_mixpanel_people_set_fxn = mocker.patch("mixpanel.Mixpanel.people_set")
-    user = User(active_user_with_permissions(fake_uuid))
+    user = User(active_user_with_permissions)
     NotifyMixpanel().track_user_profile(user)
 
     mocked_mixpanel_people_set_fxn.assert_called_once()
@@ -54,9 +55,9 @@ def test_track_mixpanel_event_when_user_is_not_present(mocker, environment_vars_
     mocked_mixpanel_track_fxn.assert_not_called()
 
 
-def test_track_mixpanel_event(mocker, environment_vars_fixtures):
+def test_track_mixpanel_event(mocker, active_user_with_permissions, environment_vars_fixtures):
     mocked_mixpanel_track_fxn = mocker.patch("mixpanel.Mixpanel.track")
-    user = User(active_user_with_permissions(fake_uuid))
+    user = User(active_user_with_permissions)
     NotifyMixpanel().track_event(user)
 
     mocked_mixpanel_track_fxn.assert_called_once()
