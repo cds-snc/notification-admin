28b1fc3ef22b387cdce00bd8e1e5539a3411f6c8
Philippe Caron <amazingphilippe@gmail.com>
2022-07-14 09:10:04 -0400

    revert auto format

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ const paths = {
   dist: "app/static/",
   templates: "app/templates/",
   npm: "node_modules/",
-  toolkit: "node_modules/govuk_frontend_toolkit/",
+  toolkit: "node_modules/govuk_frontend_toolkit/"
 };
 
 // 3. TASKS
@@ -59,25 +59,23 @@ const javascripts = () => {
     paths.src + "javascripts/menu.js",
     paths.src + "javascripts/menuOverlay.js",
     paths.src + "javascripts/scopeTabNavigation.js",
-    paths.src + "javascripts/main.js",
+    paths.src + "javascripts/main.js"
   ])
     .pipe(plugins.prettyerror())
     .pipe(
       plugins.babel({
-        presets: ["@babel/preset-env"],
+        presets: ["@babel/preset-env"]
       })
     )
-    .pipe(
-      plugins.addSrc.prepend([
-        //paths.src + 'javascripts/main.min.js',
-        paths.npm + "hogan.js/dist/hogan-3.0.2.js",
-        paths.npm + "jquery/dist/jquery.min.js",
-        paths.npm + "jquery-migrate/dist/jquery-migrate.min.js",
-        paths.npm + "query-command-supported/dist/queryCommandSupported.min.js",
-        //paths.npm + "diff-dom/diffDOM.js",
-        paths.npm + "textarea-caret/index.js",
-      ])
-    )
+    .pipe(plugins.addSrc.prepend([
+      //paths.src + 'javascripts/main.min.js',
+      paths.npm + "hogan.js/dist/hogan-3.0.2.js",
+      paths.npm + "jquery/dist/jquery.min.js",
+      paths.npm + "jquery-migrate/dist/jquery-migrate.min.js",
+      paths.npm + "query-command-supported/dist/queryCommandSupported.min.js",
+      //paths.npm + "diff-dom/diffDOM.js",
+      paths.npm + "textarea-caret/index.js"
+    ]))
     .pipe(plugins.uglify())
     .pipe(plugins.concat("all.min.js"))
     .pipe(
@@ -104,28 +102,30 @@ const images = () => {
   return src([
     paths.toolkit + "images/**/*",
     paths.template + "assets/images/**/*",
-    paths.src + "images/**/*",
+    paths.src + "images/**/*"
   ]).pipe(dest(paths.dist + "images/"));
 };
 
 const watchFiles = {
-  javascripts: (cb) => {
+  javascripts: cb => {
     watch([paths.src + "javascripts/**/*"], javascripts);
     cb();
   },
-  images: (cb) => {
+  images: cb => {
     watch([paths.src + "images/**/*"], images);
     cb();
   },
-  self: (cb) => {
+  self: cb => {
     watch(["gulpfile.js"], defaultTask);
     cb();
-  },
+  }
 };
 
 // Default: compile everything
 const defaultTask = parallel(
-  series(images),
+  series(
+    images
+  ),
   series(static_css),
   series(javascripts)
 );
