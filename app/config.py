d3cd3163359c02f5c199901b8d81924f21e3f085
Stephen Astels <sastels@gmail.com>
2022-09-28 11:30:11 -0400

    fix setting SHOW_STYLEGUIDE

--- app/config.py
+++ app/config.py
@@ -110,7 +110,7 @@ class Config(object):
     SESSION_COOKIE_SECURE = True
     SESSION_REFRESH_EACH_REQUEST = True
     SENSITIVE_SERVICES = os.environ.get("SENSITIVE_SERVICES", "")
-    SHOW_STYLEGUIDE = os.getenv("SHOW_STYLEGUIDE", "False")
+    SHOW_STYLEGUIDE = env.bool("SHOW_STYLEGUIDE", False)
 
     # Hosted graphite statsd prefix
     STATSD_HOST = os.getenv("STATSD_HOST")
