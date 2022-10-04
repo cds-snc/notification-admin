0b1ea958261d3c29c1aae282a4ac65fd004f7e4a
Stephen McMurtry <smcmurtry@gmail.com>
2022-10-04 20:06:31 +0000

    added new test

--- tests/app/main/views/test_send.py
+++ tests/app/main/views/test_send.py
@@ -3601,3 +3601,23 @@ def test_s3_send_shows_available_files(
     multiple_choise_options = [x.text.strip() for x in options]
 
     assert multiple_choise_options == expected_filenames
+
+
+@pytest.mark.parametrize(
+    "feature_flag, expected",
+    [(True, 1), (False, 0)],
+)
+def test_sms_parts_section_is_visible_depending_on_ff(
+    feature_flag, expected, client_request, service_one, fake_uuid, mock_get_service_template, mock_get_template_statistics, app_
+):
+    app_.config["FF_SMS_PARTS_UI"] = feature_flag
+
+    with client_request.session_transaction() as session:
+        session["recipient"] = "6502532223"
+        session["placeholders"] = {}
+
+    page = client_request.get("main.check_notification", service_id=service_one["id"], template_id=fake_uuid)
+
+    assert page.h1.text.strip() == "Review before sending"
+    sms_count = page.select("#sms-count")
+    assert len(list(sms_count)) == expected
