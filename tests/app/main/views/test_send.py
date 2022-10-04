800feda32156814d9d34f75888d409a74f4dd1cb
Stephen Astels <sastels@gmail.com>
2022-10-04 12:26:43 -0400

    update test

--- tests/app/main/views/test_send.py
+++ tests/app/main/views/test_send.py
@@ -2504,8 +2504,8 @@ def test_check_messages_back_link(
 @pytest.mark.parametrize(
     "num_requested,expected_msg",
     [
-        (0, "You can only send 20 text message fragments per day in trial mode. To send more messages, request to go live."),
-        (1, "You can only send 20 text message fragments per day in trial mode. To send more messages, request to go live."),
+        (0, "If a message is long, it travels in fragments. Each fragment counts toward your daily limit."),
+        (1, "If a message is long, it travels in fragments. Each fragment counts toward your daily limit."),
     ],
     ids=["none_sent", "some_sent"],
 )
@@ -2556,8 +2556,6 @@ def test_check_messages_shows_too_many_sms_messages_errors_when_FF_SPIKE_SMS_DAI
             original_file_name="valid.csv",
             _test_page_title=False,
         )
-
-    assert page.find(role="alert").find("h1").text.strip() == "Too many text message fragments"
 
     # remove excess whitespace from element
     details = page.find(role="alert").findAll("p")[0]
