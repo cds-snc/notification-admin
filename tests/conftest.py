682ea5d75fe8a0fffc7d5dd3b29db59f6a1eb64c
Stephen McMurtry <smcmurtry@gmail.com>
2022-10-04 17:18:01 +0000

    reverted unneeded change

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,16 +32,6 @@ from . import (
     user_json,
 )
 
-if os.getenv("_PYTEST_RAISE", "0") != "0":
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_exception_interact(call):
-        raise call.excinfo.value
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_internalerror(excinfo):
-        raise excinfo.value
-
 
 class ElementNotFound(Exception):
     pass
