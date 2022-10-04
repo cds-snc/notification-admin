5f33dc91bf58e296a8ffbf50ea1b5a54057439fe
Stephen Astels <sastels@gmail.com>
2022-10-04 10:41:51 -0400

    small tweak

--- app/main/views/send.py
+++ app/main/views/send.py
@@ -36,6 +36,7 @@ from app import (
     service_api_client,
     template_statistics_client,
 )
+from app.extensions import redis_client
 from app.main import main
 from app.main.forms import (
     ChooseTimeForm,
@@ -773,9 +774,8 @@ def check_messages(service_id, template_id, upload_id, row_index=2):
     data["sms_parts_requested"] = data["stats_daily"]["sms"]["requested"]
     data["sms_parts_remaining"] = current_service.sms_daily_limit - data["sms_parts_requested"]
     data["send_exceeds_daily_limit"] = data["sms_parts_to_send"] > data["sms_parts_remaining"]
-    data["send_exceeds_sms_daily_limit"] = data["sms_parts_to_send"] > data["sms_parts_remaining"]
 
-    if current_app.config["FF_SPIKE_SMS_DAILY_LIMIT"] and data["send_exceeds_sms_daily_limit"]:
+    if current_app.config["FF_SPIKE_SMS_DAILY_LIMIT"] and data["send_exceeds_daily_limit"]:
         return render_template("views/check/column-errors.html", **data)
 
     metadata_kwargs = {
