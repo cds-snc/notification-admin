6f60bfdf0df0ad1f0d6e78b766a046ece1ad5acd
Andrew <andrew.leith@cds-snc.ca>
2022-10-05 12:49:19 -0300

    Rename 'daily sms message limit' to 'daily sms fragment limit'. (#1347)
    * Rename 'daily sms message limit' to 'daily sms fragment limit'.
    
    * Update wording
    
    * Update app/templates/views/service-settings.html
    
    Using Phil's suggestion

--- app/main/views/service_settings.py
+++ app/main/views/service_settings.py
@@ -1077,7 +1077,7 @@ def set_sms_message_limit(service_id):
             flash(_("An email has been sent to service users"), "default_with_tick")
         return redirect(url_for(".service_settings", service_id=service_id))
 
-    return render_template("views/service-settings/set-message-limit.html", form=form, heading=_("Daily SMS message limit"))
+    return render_template("views/service-settings/set-message-limit.html", form=form, heading=_("Daily text fragments limit"))
 
 
 @main.route(
