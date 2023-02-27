16ba105a809335de29659407d674759d90726b50
Pat Heard <patrick.heard@cds-snc.ca>
2023-02-27 18:43:07 +0000

    fix: formatting

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,13 +1,12 @@
 (function (Modules) {
-    "use strict";
-   
-    Modules.Autocomplete = function () {
-      this.start = function (component) {
-        accessibleAutocomplete.enhanceSelectElement({
-            displayMenu: "overlay",
-            selectElement: component[0],            
-        });
-      };
-    };
+  "use strict";
 
+  Modules.Autocomplete = function () {
+    this.start = function (component) {
+      accessibleAutocomplete.enhanceSelectElement({
+        displayMenu: "overlay",
+        selectElement: component[0],
+      });
+    };
+  };
 })(window.GOVUK.Modules);
