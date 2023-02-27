018d26b482d0ef6bc01d5dd75b0657fb1e8e2f4e
Pat Heard <patrick.heard@cds-snc.ca>
2023-02-27 17:53:59 +0000

    wip: CRM integeration with trial service creation
    Add the GOV.uk accessible autocomplete field to the
    new trial service flow.

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,13 @@
+(function (Modules) {
+    "use strict";
+   
+    Modules.Autocomplete = function () {
+      this.start = function (component) {
+        accessibleAutocomplete.enhanceSelectElement({
+            displayMenu: "overlay",
+            selectElement: component[0],            
+        });
+      };
+    };
+
+})(window.GOVUK.Modules);
