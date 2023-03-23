b2ed4003ce76154a74a67eeca53a6022b9a96638
Andrew Leith <andrew.leith@cds-snc.ca>
2023-03-23 13:52:36 +0000

    chore: formatting

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,9 @@
 from flask_caching import Cache
 from notifications_utils.clients.antivirus.antivirus_client import AntivirusClient
+from notifications_utils.clients.redis.bounce_rate import RedisBounceRate
 from notifications_utils.clients.redis.redis_client import RedisClient
 from notifications_utils.clients.statsd.statsd_client import StatsdClient
 from notifications_utils.clients.zendesk.zendesk_client import ZendeskClient
-from notifications_utils.clients.redis.bounce_rate import RedisBounceRate
 
 antivirus_client = AntivirusClient()
 statsd_client = StatsdClient()
