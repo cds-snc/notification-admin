90a4c82e79914b6c160229ebd931c305cb3c79b6
Stephen McMurtry <smcmurtry@gmail.com>
2023-03-24 09:18:47 -0400

    Hide the Organisation question from "go live" flow (#1496)
    This PR hides the "what is your organisation" question from the Go Live flow if FF_SALESFORCE_CONTACT=True and we have already recorded the user's answer in the organisation_notes column in the db.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1742,7 +1742,45 @@ class GoLiveAboutServiceForm(StripWhitespaceForm):
     )
 
 
+class GoLiveAboutServiceFormNoOrg(StripWhitespaceForm):
+    purpose = TextAreaField(
+        _l("For what purpose are you using GC Notify?"),
+        validators=[DataRequired(), Length(max=2000)],
+    )
+    intended_recipients = MultiCheckboxField(
+        _l("Who are the intended recipients of notifications?"),
+        default="",
+        choices=[
+            ("internal", _l("Colleagues within your department (internal)")),
+            ("external", _l("Partners from other organisations (external)")),
+            ("public", _l("Public")),
+        ],
+        validators=[DataRequired()],
+    )
+
+
 class GoLiveAboutNotificationsForm(GoLiveAboutServiceForm):
+    notification_types = MultiCheckboxField(
+        _l("Specify the type of notifications you plan on sending."),
+        choices=[
+            ("email", _l("Email")),
+            ("sms", _l("Text message")),
+        ],
+        validators=[DataRequired()],
+    )
+    expected_volume = RadioField(
+        _l("How many notifications do you plan on sending per month?"),
+        choices=[
+            ("1-1k", _l("1 to 1,000 notifications")),
+            ("1k-10k", _l("1,000 to 10,000 notifications")),
+            ("10k-100k", _l("10,000 to 100,000 notifications")),
+            ("100k+", _l("More than 100,000 notifications")),
+        ],
+        validators=[DataRequired()],
+    )
+
+
+class GoLiveAboutNotificationsFormNoOrg(GoLiveAboutServiceFormNoOrg):
     notification_types = MultiCheckboxField(
         _l("Specify the type of notifications you plan on sending."),
         choices=[
