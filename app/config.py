918798b18dd14bfcd211186869364ce69fd3f1b2
Andrew Leith <andrew.leith@cds-snc.ca>
2023-03-30 12:07:58 +0000

    test: turn BR FF on for testing

--- app/config.py
+++ app/config.py
@@ -193,6 +193,7 @@ class Test(Development):
     GC_ARTICLES_API = "articles.alpha.canada.ca/notification-gc-notify"
     FF_SPIKE_SMS_DAILY_LIMIT = False
     FF_SMS_PARTS_UI = False
+    FF_BOUNCE_RATE_V1 = True
 
 
 class Production(Config):
