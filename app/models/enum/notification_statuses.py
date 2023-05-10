bec373c10776f478faa59bcb043a71296be770ba
wbanks <william.banks@cds-snc.ca>
2023-05-10 14:17:43 -0400

    Add one offs to review email list page
    - Add some enums for template types and notification statuses

---
+++ app/models/enum/notification_statuses.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class NotificationStatuses(Enum):
+    CANCELLED = 'cancelled'
+    CREATED = 'created'
+    DELIVERED = 'delivered'
+    FAILED = 'failed'
+    PENDING = 'pending'
+    PENDING_VIRUS_CHECK = 'pending-virus-check'
+    PERMANENT_FAILURE = 'permanent-failure'
+    PII_CHECK_FAILED = 'pii-check-failed'
+    RETURNED_LETTER = 'returned-letter'
+    SENDING = "sending"
+    SENT = 'sent'
+    TECHNICAL_FAILURE = 'technical-failure'
+    TEMPORARY_FAILURE = 'temporary-failure'
+    VALIDATION_FAILED = 'validation-failed'
+    VIRUS_SCAN_FAILED = 'virus-scan-failed'
