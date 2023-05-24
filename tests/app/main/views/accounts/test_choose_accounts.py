b7005e26b06f28c7649b8e7e9f336ba980043bfe
Stephen McMurtry <smcmurtry@gmail.com>
2023-05-24 21:33:01 +0000

    format

--- tests/app/main/views/accounts/test_choose_accounts.py
+++ tests/app/main/views/accounts/test_choose_accounts.py
@@ -5,12 +5,7 @@ import pytest
 from bs4 import BeautifulSoup
 from flask import url_for
 
-from tests.conftest import (
-    SERVICE_ONE_ID,
-    SERVICE_TWO_ID,
-    create_api_user_active,
-    normalize_spaces,
-)
+from tests.conftest import SERVICE_ONE_ID, SERVICE_TWO_ID, normalize_spaces
 
 OS1, OS2, OS3, S1, S2, S3 = repeat(uuid.uuid4(), 6)
 
