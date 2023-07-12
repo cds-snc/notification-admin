649125e330404b2272b4551190f1750582c23e54
Stephen Astels <sastels@gmail.com>
2023-07-12 12:58:15 -0400

    add explicit test of NOTIFY_ENVIRONMENT

--- app/status/views/debugging.py
+++ app/status/views/debugging.py
@@ -8,9 +8,13 @@ def debug():
     """A route we can hit to help us debug in AWS. Currently just raises an exception to test logging.
 
     Raises:
-        Exception: if you pass the correct key as a query param
+        Exception: if you pass the correct key as a query param and you're in a non-production environment
     """
-    if current_app.config["DEBUG_KEY"] and current_app.config["DEBUG_KEY"] == request.args.get("key", None):
+    if (
+        current_app.config["NOTIFY_ENVIRONMENT"] in ["development", "test", "staging", "scratch"]
+        and current_app.config["DEBUG_KEY"]
+        and current_app.config["DEBUG_KEY"] == request.args.get("key", None)
+    ):
         raise Exception("Debugging")
     else:
         abort(404)
