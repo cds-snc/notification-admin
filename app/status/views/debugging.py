d262a4cae33aa0201b5bd8260683ed4d342b0bea
Stephen Astels <sastels@gmail.com>
2023-07-12 13:22:47 -0400

    switch to ALLOW_DEBUG_ROUTE flag

--- app/status/views/debugging.py
+++ app/status/views/debugging.py
@@ -11,7 +11,7 @@ def debug():
         Exception: if you pass the correct key as a query param and you're in a non-production environment
     """
     if (
-        current_app.config["NOTIFY_ENVIRONMENT"] in ["development", "test", "staging", "scratch"]
+        current_app.config["ALLOW_DEBUG_ROUTE"]
         and current_app.config["DEBUG_KEY"]
         and current_app.config["DEBUG_KEY"] == request.args.get("key", None)
     ):
