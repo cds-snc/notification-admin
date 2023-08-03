fb6a11e01ad0b133fd6a0fc59302734f364f9113
Andrew Leith <andrew.leith@cds-snc.ca>
2023-08-03 12:28:24 +0000

    tests: ensure flash message is displayed when settings are updated

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -789,6 +789,10 @@ def test_should_redirect_after_service_name_confirmation(
         ),
     )
 
+    # Ensure flash message is set
+    with client_request.session_transaction() as session:
+        assert session["_flashes"][0][1] == "Setting updated"
+
     mock_update_service.assert_called_once_with(
         SERVICE_ONE_ID,
         name=service_new_name,
@@ -831,7 +835,11 @@ def test_should_redirect_after_service_email_from_confirmation(
             service_id=SERVICE_ONE_ID,
         ),
     )
-
+    
+    # Ensure flash message is set
+    with client_request.session_transaction() as session:
+        assert session["_flashes"][0][1] == "Setting updated"
+        
     mock_update_service.assert_called_once_with(SERVICE_ONE_ID, email_from=email_safe(service_new_email_from))
     assert mock_verify_password.called is True
 
@@ -3308,6 +3316,11 @@ def test_switch_service_enable_letters(
         _data={"enabled": posted_value},
         _expected_redirect=url_for("main.service_settings", service_id=service_one["id"]),
     )
+
+    # Ensure flash message is set
+    with client_request.session_transaction() as session:
+        assert session["_flashes"][0][1] == "Setting updated"
+
     assert set(mocked_fn.call_args[1]["permissions"]) == set(expected_updated_permissions)
     assert mocked_fn.call_args[0][0] == service_one["id"]
 
