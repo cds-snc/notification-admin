8c8f392efdbca86e56ffe4bd1e3c158422258fb0
Andrew Leith <andrew.leith@cds-snc.ca>
2023-09-26 14:10:17 +0000

    chore: formatting

--- app/assets/javascripts/lexical/index.js
+++ app/assets/javascripts/lexical/index.js
@@ -5,11 +5,13 @@ import { RichTextEditor } from "./RichTextEditor";
 import "./styles.css";
 
 // read properties from the script tag
-const scriptTag = document.currentScript; 
+const scriptTag = document.currentScript;
 const data = scriptTag.dataset;
 
-if (data.id === undefined || data.id === '') {
-  console.error("rich_text_component (lexical): A data-id property is required on the script tag to tell the component which element to render into.")
+if (data.id === undefined || data.id === "") {
+  console.error(
+    "rich_text_component (lexical): A data-id property is required on the script tag to tell the component which element to render into.",
+  );
 }
 
 const rootElement = document.getElementById(`root_${data.id}`);
