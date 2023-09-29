8ca69c6b1a6f1512df5093904d33709321e54e91
Andrew Leith <andrew.leith@cds-snc.ca>
2023-09-29 14:03:07 +0000

    chore: fix test comments

--- tests/app/main/views/test_google_analytics.py
+++ tests/app/main/views/test_google_analytics.py
@@ -3,12 +3,12 @@ def test_google_analytics_logged_in(
     active_cds_user_with_permissions,
     active_user_with_permissions,
 ):
-    # Ensure GA scripts are loaded for non-cds users
+    # Ensure GA scripts ARE NOT loaded for cds users
     client_request.login(active_cds_user_with_permissions)
     page = client_request.get("main.welcome")
     assert "googletagmanager" not in str(page.contents)
 
-    # Ensure GA scripts are NOT loaded for cds users
+    # Ensure GA scripts ARE loaded for non-cds users
     client_request.login(active_user_with_permissions)
     page = client_request.get("main.welcome")
     assert "googletagmanager" in str(page.contents)
@@ -25,7 +25,6 @@ def test_google_analytics_logged_out(
     page = client_request.get("main.sign_in")
     assert "googletagmanager" in str(page.contents)
 
-    # Ensure GA scripts are NOT loaded for cds usersclient_request.login(active_cds_user_with_permissions)
     client_request.login(active_user_with_permissions)
     client_request.logout()
     page = client_request.get("main.sign_in")
