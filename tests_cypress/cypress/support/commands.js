88b0130a9d0e11463303298ed5cade4b3fa27546
Andrew Leith <andrew.leith@cds-snc.ca>
2023-11-17 16:31:19 -0400

    fix: make tests way faster and impervious to rate limits
    - use the waf-secret header to avoid rate limiting (needs a WAF rule change to work staging/prod)
    - make tests smarter by not checking the same links or the same svgs more than once

--- tests_cypress/cypress/support/commands.js
+++ tests_cypress/cypress/support/commands.js
@@ -1,3 +1,5 @@
+import config from "../../config";
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -24,8 +26,18 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+// keep track of what we test so we dont test the same thing twice
+let links_checked = [];
+let svgs_checked = [];
+
 Cypress.Commands.add('a11yScan', (url, options={ a11y: true, htmlValidate: true, deadLinks: true, mimeTypes: true }) => {
-    cy.visit(url)
+    const current_hostname = config.Hostnames.Admin;
+    // bypass rate limiting
+    cy.intercept(`${current_hostname}/*`, (req) => {
+        req.headers['waf-secret'] = Cypress.env(config.CONFIG_NAME).WAF_SECRET
+    });
+
+    cy.visit(url);
 
     // 1. validate a11y rules using axe dequeue
     if (options.a11y) {
@@ -47,10 +59,35 @@ Cypress.Commands.add('a11yScan', (url, options={ a11y: true, htmlValidate: true,
     // 3. check for dead links
     if (options.deadLinks) {
         cy.get('body').within(() => {
+            let checked = 0;
+
             cy.get('a').each((link) => {
-                if (link.prop('href').startsWith('mailto') || link.prop('href').startsWith('/set-lang') || link.prop('href').startsWith('#')) return;
-                cy.request(link.prop('href'));
+                if (link.prop('href').startsWith('mailto') || link.prop('href').includes('/set-lang') || link.prop('href').includes(url)) return;
+
+                const check_url = link.prop('href');
+
+                // skip if its already been checked
+                if (links_checked.includes(check_url)) return;
+
+                // bypass rate limiting
+                if (check_url.includes(current_hostname)) {
+                    cy.request({
+                        url: check_url,
+                        headers: { 'waf-secret': Cypress.env(config.CONFIG_NAME).WAF_SECRET }
+                    }).as('link');
+                }
+                else {
+                    cy.request(link.prop('href')).as('link');
+                }
+
+                links_checked.push(check_url);
+                checked++;
+            }).as('links');
+
+            cy.get('@links').then((links) => {
+                cy.log('links checked', checked);
             });
+
         });
     }
 
@@ -58,10 +95,22 @@ Cypress.Commands.add('a11yScan', (url, options={ a11y: true, htmlValidate: true,
     if (options.mimeTypes) {
         const svg_mime_type = 'image/svg+xml';
         cy.get('body').within(() => {
+            let checked = 0;
             cy.get('img').each((link) => {
-                if (link.prop('src').endsWith('.svg')) {
+                const svg = link.prop('src');
+                if (svg.endsWith('.svg')) {
+                    // skip if its already been checked
+                    if (svgs_checked.includes(svg)) return;
+
                     cy.request(link.prop('src')).its('headers').its('content-type').should('include', svg_mime_type);
+
+                    svgs_checked.push(svg);
+                    checked++;
                 }
+            }).as('svgs');
+
+            cy.get('@svgs').then((links) => {
+                cy.log('svgs checked', checked);
             });
         });
     }
