027f6d498ef7b37d006a7edbcd90c20f7b5a283c
Philippe Caron <amazingphilippe@gmail.com>
2024-01-15 13:18:28 -0500

    translations

--- app/main/views/styleguide.py
+++ app/main/views/styleguide.py
@@ -1,4 +1,5 @@
 from flask import abort, current_app, render_template
+from flask_babel import _
 from flask_wtf import FlaskForm as Form
 from notifications_utils.template import Template
 from wtforms import FileField, PasswordField, StringField, TextAreaField, validators
@@ -31,33 +32,17 @@ def styleguide():
 
 @main.route("/_rte")
 def rte():
+    from app import get_current_locale
+    
+    lang = get_current_locale(current_app)
+    template_content = "Annual newsletter\n\nWe hope this newsletter finds you well! As we dive into the new year, we want to ensure you're up to speed with the latest happenings. Here's a quick recap!\n\nThings You May Have Missed:\n\nThing 1\n\nThing 2\n\nSteps to Sign Up:\n\nVisit our website \n\nLook for the Newsletter Sign-Up section on the homepage.\n\nEnter your email address in the provided field.\n\nClick Subscribe to start receiving our regular updates directly in your inbox.\n\nMore Information:\n\nFor more details on our products, services, or any other inquiries, feel free to reach out to our dedicated support team at [support-email] or visit our FAQ page [FAQ-link]. We value your feedback and are here to assist you in any way we can."
+
+    if lang == "fr":
+        template_content = "Infolettre annuelle\n\nNous espérons que cette infolettre vous trouvera en bonne santé! Voici un aperçu des derniers événements pour que vous soyez à jour. \n\nCe que vous avez peut-être manqué:\n\nChose 1\n\nChose 2\n\nPour vous inscrire:\n\nVisitez notre site Web\n\nTrouvez le lien d’inscription à l’infolettre sur la page d’accueil\n\nEntrez votre adresse courriel\n\nCliquez sur S’inscrire pour recevoir les mises à jour dans votre boîte de façon régulière\n\nPlus d’information\n\nPour plus d'informations sur nos produits, services ou tout autre question, n’hésitez pas à contacter notre équipe d’assistance au [courriel d’assistance] ou visiter notre FAQ [lien FAQ]. Nous apprécions vos commentaires et sommes toujours à votre disposition pour vous aider."
+
     template = {
         "id": "fable_test",
-        "data": """Annual newsletter
-
-We hope this newsletter finds you well! As we dive into the new year, we want to ensure you're up to speed with the latest happenings. Here's a quick recap!
-
-Things You May Have Missed:
-
-Thing 1
-
-Thing 2
-
-\r\n\r\n\r\n
-
-Steps to Sign Up:
-
-Visit our website 
-
-Look for the Newsletter Sign-Up section on the homepage.
-
-Enter your email address in the provided field.
-
-Click Subscribeto start receiving our regular updates directly in your inbox.
-
-More Information:
-
-For more details on our products, services, or any other inquiries, feel free to reach out to our dedicated support team at [support-email] or visit our FAQ page [FAQ-link]. We value your feedback and are here to assist you in any way we can.""",
+        "data": template_content,
     }
 
     return render_template("views/rte.html", template=template)
