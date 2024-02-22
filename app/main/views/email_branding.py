394b99d0d7a6feecc4a19ae89095c663fb2c1ef7
Andrew Leith <andrew.leith@cds-snc.ca>
2024-02-22 14:35:59 +0000

    feat(branding): add new routes/logic for branding

--- app/main/views/email_branding.py
+++ app/main/views/email_branding.py
@@ -1,8 +1,9 @@
-from flask import current_app, redirect, render_template, session, url_for
+from app import current_service
+from flask import current_app, flash, redirect, render_template, session, url_for
 
 from app import email_branding_client
 from app.main import main
-from app.main.forms import SearchByNameForm, ServiceUpdateEmailBranding
+from app.main.forms import BrandingGOCForm, SearchByNameForm, ServiceUpdateEmailBranding
 from app.s3_client.s3_logo_client import (
     TEMP_TAG,
     delete_email_temp_file,
@@ -11,8 +12,8 @@ from app.s3_client.s3_logo_client import (
     persist_logo,
     upload_email_logo,
 )
-from app.utils import get_logo_cdn_domain, user_is_platform_admin
-
+from app.utils import get_logo_cdn_domain, user_has_permissions, user_is_platform_admin
+from flask_babel import _
 
 @main.route("/email-branding", methods=["GET", "POST"])
 @user_is_platform_admin
@@ -131,3 +132,45 @@ def create_email_branding(logo=None):
         cdn_url=get_logo_cdn_domain(),
         logo=logo,
     )
+
+
+### NEW BRANDING ###
+@main.route("/services/<service_id>/edit-branding", methods=["GET", "POST"])
+@user_has_permissions("manage_service")
+def edit_branding_settings(service_id):
+    # redirect back to start if branding not in session
+    if not session.get("branding"):
+        return redirect(url_for("main.view_branding_settings", service_id=service_id))        
+    
+    cdn_url = get_logo_cdn_domain()
+    default_en_filename = "https://{}/gov-canada-en.svg".format(cdn_url)
+    default_fr_filename = "https://{}/gov-canada-fr.svg".format(cdn_url)
+    choices = [
+        (default_en_filename),
+        (default_fr_filename),
+    ]
+    form = BrandingGOCForm()
+
+    if form.validate_on_submit():
+        default_branding_is_french = form.data["goc_branding"] == BrandingGOCForm.DEFAULT_FR[0]
+        current_service.update(email_branding=None, default_branding_is_french=default_branding_is_french)
+
+        flash(_("Setting updated"), "default_with_tick")
+        return redirect(url_for("main.view_branding_settings", service_id=service_id))
+    
+    return render_template("views/email-branding/branding-goc.html", choices=choices, form=form)
+
+@main.route("/services/<service_id>/review-pool", methods=["GET", "POST"])
+@user_has_permissions("manage_service")
+def review_branding_pool(service_id):
+    return render_template("views/email-branding/branding-pool.html")
+
+@main.route("/services/<service_id>/branding-request", methods=["GET", "POST"])
+@user_has_permissions("manage_service")
+def create_branding_request(service_id):
+    return render_template("views/email-branding/branding-request.html")
+
+@main.route("/services/<service_id>/preview-branding", methods=["GET", "POST"])
+@user_has_permissions("manage_service")
+def preview_branding(service_id):
+    return render_template("views/email-branding/branding-preview.html")
