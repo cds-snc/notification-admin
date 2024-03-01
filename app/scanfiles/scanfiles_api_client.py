a752378c7435e5fb06eb28166e24c5a55711b967
Andrew Leith <andrew.leith@cds-snc.ca>
2024-03-01 17:26:46 +0000

    chore: formatting

--- app/scanfiles/scanfiles_api_client.py
+++ app/scanfiles/scanfiles_api_client.py
@@ -22,44 +22,44 @@ class ScanFilesApiClient:
         self.auth_token = auth_token
 
     def is_unsafe(self, file):
-            """
-            Checks if a file is considered unsafe by sending it to the scanfiles API.
+        """
+        Checks if a file is considered unsafe by sending it to the scanfiles API.
 
-            Args:
-                file (bytes): The path to the file to be scanned.
+        Args:
+            file (bytes): The path to the file to be scanned.
 
-            Returns:
-                bool: True if the file is considered unsafe, False otherwise.
-            """
-            data = {"file": file}
-            headers = {
-                "Authorization": self.auth_token,
-            }
+        Returns:
+            bool: True if the file is considered unsafe, False otherwise.
+        """
+        data = {"file": file}
+        headers = {
+            "Authorization": self.auth_token,
+        }
 
-            try:
-                response = requests.post(self.scanfiles_url, files=data, headers=headers, timeout=15)
-            except Exception as e:
-                current_app.logger.debug("ScanFilesApiClient: Failed to scan file. Error: {}".format(str(e)))
-                return False
+        try:
+            response = requests.post(self.scanfiles_url, files=data, headers=headers, timeout=15)
+        except Exception as e:
+            current_app.logger.debug("ScanFilesApiClient: Failed to scan file. Error: {}".format(str(e)))
+            return False
 
-            if response.status_code == 200:
-                response_data = response.json()
-                verdict = response_data["verdict"]
+        if response.status_code == 200:
+            response_data = response.json()
+            verdict = response_data["verdict"]
 
-                current_app.logger.debug(
-                    "ScanFilesApiClient: request successful. Scan result: {}. Response data: {}".format(
-                        response_data["verdict"], response_data
-                    )
+            current_app.logger.debug(
+                "ScanFilesApiClient: request successful. Scan result: {}. Response data: {}".format(
+                    response_data["verdict"], response_data
                 )
+            )
 
-                if verdict == ScanVerdicts.SUSPICIOUS.value or verdict == ScanVerdicts.MALICIOUS.value:
-                    return True
+            if verdict == ScanVerdicts.SUSPICIOUS.value or verdict == ScanVerdicts.MALICIOUS.value:
+                return True
 
-                # all other verdicts
-                return False
-            else:
-                current_app.logger.debug("ScanFilesApiClient: Failed to scan file. Response {}".format(str(response)))
-                return False
+            # all other verdicts
+            return False
+        else:
+            current_app.logger.debug("ScanFilesApiClient: Failed to scan file. Response {}".format(str(response)))
+            return False
 
 
 scanfiles_api_client = ScanFilesApiClient()
