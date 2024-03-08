9732433e622460c558128c1cf81d8fed724fe231
Andrew Leith <andrew.leith@cds-snc.ca>
2024-03-08 09:37:04 -0400

    feat(login): add command to wrap login into session so it can persist when possible

--- tests_cypress/cypress/support/commands.js
+++ tests_cypress/cypress/support/commands.js
@@ -1,30 +1,5 @@
 import config from "../../config";
-
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { LoginPage } from "../Notify/Admin/Pages/all";
 
 // keep track of what we test so we dont test the same thing twice
 let links_checked = [];
@@ -120,3 +95,8 @@ Cypress.Commands.add('getByTestId', (selector, ...args) => {
     return cy.get(`[data-testid=${selector}]`, ...args)
 });
   
+Cypress.Commands.add('login', (username, password) => {
+    cy.session([username, password], () => {
+        LoginPage.Login(username, password);
+    });
+});
