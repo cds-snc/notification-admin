efc65c31fec470f71ee273bb0cdaf58d165be25d
wbanks <william.banks@cds-snc.ca>
2024-03-21 10:47:25 -0400

    Add more tests for review-pool

--- tests_cypress/cypress/e2e/admin/branding/review-pool.cy.js
+++ tests_cypress/cypress/e2e/admin/branding/review-pool.cy.js
@@ -2,6 +2,7 @@
 
 import config from "../../../../config";
 import { ReviewPoolPage } from "../../../Notify/Admin/Pages/AllPages";
+import { Admin } from "../../../Notify/NotifyAPI";
 
 describe("Review Pool", () => {
 
@@ -14,24 +15,32 @@ describe("Review Pool", () => {
         cy.visit(config.Hostnames.Admin + `/services/${config.Services.Cypress}/review-pool`);
     });
 
+    // after(() => {
+    //     // Restore the test service's org to the default
+    //     Admin.LinkOrganisationToService({ orgId: config.Organisations.DEFAULT_ORG_ID, serviceId: config.Services.Cypress });
+    // });
+
     /// Navigation functionality ///
     it("Loads review pool page", () => {
         cy.contains('h1', 'Select alternate logo').should('be.visible');
     });
 
+    // BROKEN CURRENTLY
     // it('Returns to edit branding page when back link is clicked', () => {
     //     ReviewPoolPage.ClickBackLink();
     //     cy.get('h1').contains('Change your branding').should('be.visible');
     // });
 
+
+    it('Loads request branding page when [request a new logo] link is clicked', () => {
+        ReviewPoolPage.ClickRequestNewLogoLink();
+        cy.get('h1').contains('Request a new logo').should('be.visible');
+    });
+
     context("Service's org has no custom branding", () => {
         before(() => {
             // Change the org to one that has no branding
-
-        });
-
-        after(() => {
-            // Set the org back to one that has branding
+            Admin.LinkOrganisationToService({ orgId: config.Organisations.NO_CUSTOM_BRANDING_ORG_ID, serviceId: config.Services.Cypress });
         });
 
         it('Displays a banner indicating there is no custom branding associated with their organisation and a link to request custom branding', () => {
@@ -42,17 +51,19 @@ describe("Review Pool", () => {
     });
 
 
-    // context("Service's org has custom branding", () => {
-    //     /// Main functionality ///
-    //     it('Loads request branding page when [request a new logo] link is clicked', () => {
-    //         ReviewPoolPage.ClickRequestNewLogoLink();
-    //         cy.get('h1').contains('Request a new logo').should('be.visible');
-    //     });
-    // });
+    context("Service's org has custom branding", () => {
+        /// Main functionality ///
+        before(() => {
+            // Make sure the org is set to the default one that has custom branding
+            Admin.LinkOrganisationToService({ orgId: config.Organisations.DEFAULT_ORG_ID, serviceId: config.Services.Cypress });
+        });
+
+        it('Displays a list of available logos', () => {
+            // Associate custom branding with the service's org
+            ReviewPoolPage.Components.AvailableLogoRadios().should('not.be.empty');
+        });
+    });
 
 
-    // it('Displays a list of available logos', () => {
-    //     // Associate custom branding with the service's org
-    //     ReviewPoolPage.Components.AvailableLogoRadios().should('not.be.empty');
-    // });
+
 });
