9fb14bbcca59bf33db0368c20a56ea554528ad8b
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2024-04-03 19:35:44 +0000

    Try fixing autocomplete for org

--- app/config.py
+++ app/config.py
@@ -142,7 +142,7 @@ class Config(object):
     SCANFILES_AUTH_TOKEN = os.environ.get("SCANFILES_AUTH_TOKEN", "")
 
     # FEATURE FLAGS
-    FF_SALESFORCE_CONTACT = env.bool("FF_SALESFORCE_CONTACT", False)
+    FF_SALESFORCE_CONTACT = env.bool("FF_SALESFORCE_CONTACT", True)
     FF_SALESFORCE_CONTACT = env.bool("FF_SALESFORCE_CONTACT", False)
     FF_NEW_BRANDING = env.bool("FF_NEW_BRANDING", False)
     NO_BRANDING_ID = os.environ.get("NO_BRANDING_ID", "0af93cf1-2c49-485f-878f-f3e662e651ef")
