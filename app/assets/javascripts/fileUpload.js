9a8fd2f19c9d574eb87e65353b04ca3b705f2879
Philippe Caron <amazingphilippe@gmail.com>
2024-04-23 15:17:49 -0400

    file inputs a and b

--- app/assets/javascripts/fileUpload.js
+++ app/assets/javascripts/fileUpload.js
@@ -11,7 +11,12 @@
         .addClass("button-red")
         .text(this.cancelText)
         .attr("id", "#file-upload-button");
-
+    
+        this.showFileName = () => { 
+          let fileInput = document.querySelector("input#file_upload")
+          $("#file-description", this.$form).text(`${window.polyglot.t("File name:")} ${fileInput.files[0].name}`);
+        }
+ 
     this.start = function (component) {
       this.$form = $(component);
 
@@ -20,7 +25,7 @@
 
       // Need to put the event on the container, not the input for it to work properly
       this.$form.on("change", ".file-upload-field", () =>
-        this.showCancelButton(),
+        this.showCancelButton() && this.showFileName(),
       );
     };
   };
