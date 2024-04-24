160cfb81984d0cdb0823f9edaab9e04b0240c10f
Philippe Caron <amazingphilippe@gmail.com>
2024-04-24 17:55:45 +0000

    Button links accessibilty

---
+++ app/assets/javascripts/button.js
@@ -0,0 +1,26 @@
+(function (Modules) {
+  "use strict";
+
+  Modules.Button = function () {
+    this.start = function (component) {
+      /** Handle links that look like buttons
+       *
+       * - JavaScript makes the space key activate the link
+       * - role="button" is added to the link so that it’s recognised and announced as button by voice dictation and screen reader software.
+       * - draggable="false" is added to the link so that people with dexterity impairments don’t accidentally drag the link instead of cliking it.
+       */
+
+      // Listen to keydown on component
+      $(component).on("keydown", (event) => {
+        let target = event.target;
+
+        // If role is a button, and SPACE is pressed...
+        if (target.getAttribute("role") === "button" && event.which === 32) {
+          // Prevent default (scroll down), and click the button.
+          event.preventDefault();
+          target.click();
+        }
+      });
+    };
+  };
+})(window.GOVUK.Modules);
