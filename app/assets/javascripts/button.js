b8e8ad87985a1d1ce42046a5daa716b9238a99ba
Andrew Leith <andrew.leith@cds-snc.ca>
2024-05-03 20:02:04 +0000

    feat(link-button): enhance button in JS

--- app/assets/javascripts/button.js
+++ app/assets/javascripts/button.js
@@ -1,26 +1,23 @@
-(function (Modules) {
+/**
+ * Adds role and draggable attributes to all anchor elements with the class "button".
+ * Allows buttons to be triggered by pressing the space key.
+ */
+(function () {
   "use strict";
 
-  Modules.Button = function () {
-    this.start = function (component) {
-      /** Handle links that look like buttons
-       *
-       * - JavaScript makes the space key activate the link
-       * - role="button" is added to the link so that it’s recognised and announced as button by voice dictation and screen reader software.
-       * - draggable="false" is added to the link so that people with dexterity impairments don’t accidentally drag the link instead of cliking it.
-       */
+  /**
+   * Adds attributes and keydown listener
+   * @param {HTMLElement} button - The button element.
+   */
+  document.querySelectorAll("a.button").forEach((button) => {
+    button.setAttribute("role", "button");
+    button.setAttribute("draggable", "false");
 
-      // Listen to keydown on component
-      $(component).on("keydown", (event) => {
-        let target = event.target;
-
-        // If role is a button, and SPACE is pressed...
-        if (target.getAttribute("role") === "button" && event.key === " ") {
-          // Prevent default (scroll down), and click the button.
-          event.preventDefault();
-          target.click();
-        }
-      });
-    };
-  };
-})(window.GOVUK.Modules);
+    button.addEventListener('keydown', (event) => {
+      if (event.key === ' ') {
+        event.preventDefault();
+        button.click();
+      }
+    });
+  });
+})();
