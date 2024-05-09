568260e14af9961b678a4388f65b38774d5a47b9
Andrew Leith <andrew.leith@cds-snc.ca>
2024-05-09 16:02:18 +0000

    tests(sign-out): add tests for session timeout: 1. Redirect does not occur when not logged in 2. Redirect does occur (on app and public pages) when logged in 3. Explicit logout shows banner message

---
+++ tests_cypress/cypress/e2e/admin/sign_out/sign_out.js
@@ -0,0 +1,37 @@
+import { LoginPage } from "../../../Notify/Admin/Pages/AllPages";
+
+const REDIRECT_LOCATION = '/sign-in?timeout=true';
+const SESSION_TIMEOUT_MS = 7 * 60 * 60 * 1000 + 55 * 60 * 1000; // 7 hours 55 minutes
+const vistPageAndFastForwardTime = (page = '/') => {
+  cy.clock();
+  cy.visit(page);
+  cy.tick(SESSION_TIMEOUT_MS);
+};
+
+describe('Sign out', () => {
+
+  it('Does not redirect to session timeout page when logged out', () => {
+    cy.clearCookie('notify_admin_session'); // cle
+    vistPageAndFastForwardTime();
+
+    // asserts
+    cy.url().should('not.include', REDIRECT_LOCATION);
+  });
+
+  it('Redirects to session timeout page when logged in (multiple pages)', () => {
+    ['/home', '/features'].forEach((page) => {
+      LoginPage.Login(Cypress.env('NOTIFY_USER'), Cypress.env('NOTIFY_PASSWORD'));
+      vistPageAndFastForwardTime(page);
+
+      // asserts
+      cy.url().should('include', REDIRECT_LOCATION);
+      cy.get('h1').should('contain', 'You need to sign in again');
+    });
+  });
+
+  it('Displays banner on explicit logout', () => {
+    cy.visit('/sign-out');
+    cy.get('.banner-default-with-tick').should('be.visible');
+  });
+
+});
