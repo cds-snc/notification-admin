3194e157248d74d8902582e31050c68cdcca5c3c
Andrew Leith <andrew.leith@cds-snc.ca>
2024-05-14 15:49:31 -0300

    fix(app_pages tests): simplify code and rely on cy.session for login

--- tests_cypress/cypress/e2e/admin/a11y/app_pages.cy.js
+++ tests_cypress/cypress/e2e/admin/a11y/app_pages.cy.js
@@ -79,13 +79,9 @@ const pages = [
 ];
 
 describe(`A11Y - App pages [${config.CONFIG_NAME}]`, () => {
-  retryableBefore(() => {
-    cy.login(Cypress.env("NOTIFY_USER"), Cypress.env("NOTIFY_PASSWORD"));
-    cy.task("log", "Running against:" + Cypress.config("baseUrl"));
-  });
-
   for (const page of pages) {
     it(`${page.name}`, () => {
+      cy.login(Cypress.env("NOTIFY_USER"), Cypress.env("NOTIFY_PASSWORD"));
       cy.a11yScan(page.route, {
         a11y: true,
         htmlValidate: true,
@@ -96,36 +92,3 @@ describe(`A11Y - App pages [${config.CONFIG_NAME}]`, () => {
   }
 });
 
-/**
- * A `before()` alternative that gets run when a failing test is retried.
- *
- * By default cypress `before()` isn't run when a test below it fails
- * and is retried. Because we use `before()` as a place to setup state
- * before running assertions inside `it()` this means we can't make use
- * of cypress retry functionality to make our suites more reliable.
- *
- * https://github.com/cypress-io/cypress/issues/19458
- * https://stackoverflow.com/questions/71285827/cypress-e2e-before-hook-not-working-on-retries
- */
-function retryableBefore(fn) {
-  let shouldRun = true;
-
-  // we use beforeEach as cypress will run this on retry attempt
-  // we just abort early if we detected that it's already run
-  beforeEach(() => {
-    if (!shouldRun) return;
-    shouldRun = false;
-    fn();
-  });
-
-  // When a test fails we flip the `shouldRun` flag back to true
-  // so when cypress retries and runs the `beforeEach()` before
-  // the test that failed, we'll run the `fn()` logic once more.
-  Cypress.on("test:after:run", (result) => {
-    if (result.state === "failed") {
-      if (result.currentRetry < result.retries) {
-        shouldRun = true;
-      }
-    }
-  });
-}
