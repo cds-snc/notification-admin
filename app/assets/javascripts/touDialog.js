c13d48e5a179309335be69b04a0d74eb217d7cd8
Andrew Leith <andrew.leith@cds-snc.ca>
2024-05-29 17:29:37 +0000

    chore: formatting

--- app/assets/javascripts/touDialog.js
+++ app/assets/javascripts/touDialog.js
@@ -2,84 +2,86 @@
  * Initializes the Terms of Use dialog and adds event listeners.
  */
 (function () {
-    "use strict";
+  "use strict";
 
-    // HTML elements
-    const dialog = document.getElementById("tou-dialog");
-    const err_msg = document.getElementById("terms-error");
-    const terms = document.getElementById("tou-terms");
-    const accept = document.getElementById("tou-accept");
-    const form = document.querySelector("#tou-dialog form");
-    const trigger = document.getElementById("tou-dialog-trigger");
-    const form_agree = document.querySelector("input[name='tou_agreed']");
-    const close_button = document.getElementById("tou-close");
+  // HTML elements
+  const dialog = document.getElementById("tou-dialog");
+  const err_msg = document.getElementById("terms-error");
+  const terms = document.getElementById("tou-terms");
+  const accept = document.getElementById("tou-accept");
+  const form = document.querySelector("#tou-dialog form");
+  const trigger = document.getElementById("tou-dialog-trigger");
+  const form_agree = document.querySelector("input[name='tou_agreed']");
+  const close_button = document.getElementById("tou-close");
 
-    // Close the dialog when the user clicks outside of it
-    // dialog.addEventListener('mousedown', function(event) {
-    //   // Check if the click event's target is the dialog itself
-    //   if (event.target === dialog) {
-    //     // If it is, close the dialog
-    //     closeModal();
-    //   }
-    // });
+  // Close the dialog when the user clicks outside of it
+  // dialog.addEventListener('mousedown', function(event) {
+  //   // Check if the click event's target is the dialog itself
+  //   if (event.target === dialog) {
+  //     // If it is, close the dialog
+  //     closeModal();
+  //   }
+  // });
 
-    // Show the dialog when the trigger button is clicked
-    trigger.addEventListener("click", function () {
-        openModal();
-    });
+  // Show the dialog when the trigger button is clicked
+  trigger.addEventListener("click", function () {
+    openModal();
+  });
 
-    // If the user tries to close the dialog using the escape key, show a message
-    // that the terms must be accepted before dismissing the dialog
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "Escape") {
-            closeModal();
-        }
-    });
+  // If the user tries to close the dialog using the escape key, show a message
+  // that the terms must be accepted before dismissing the dialog
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  });
 
-    // Add agree button click listener that closes the modal and sets the form_agree value to true
-    accept.addEventListener("click", function (e) {
-        form_agree.value = "true";
-        closeModal();
-    });
+  // Add agree button click listener that closes the modal and sets the form_agree value to true
+  accept.addEventListener("click", function (e) {
+    form_agree.value = "true";
+    closeModal();
+  });
 
-    // Add close button click listener that closes the modal
-    close_button.addEventListener("click", function (e) {
-        closeModal();
-    });
+  // Add close button click listener that closes the modal
+  close_button.addEventListener("click", function (e) {
+    closeModal();
+  });
 
-    // add event handler that enables submit button when the user has scrolled to the bottom of #tou-terms
-    terms.addEventListener("scroll", function () {
-        // set disabled=true and remove class disabled when div is fully scrolled
-        if (terms.scrollHeight - terms.scrollTop <= terms.clientHeight + 5) {
-            accept.disabled = false;
-            accept.classList.remove('disabled');
-        }
-    });
+  // add event handler that enables submit button when the user has scrolled to the bottom of #tou-terms
+  terms.addEventListener("scroll", function () {
+    // set disabled=true and remove class disabled when div is fully scrolled
+    if (terms.scrollHeight - terms.scrollTop <= terms.clientHeight + 5) {
+      accept.disabled = false;
+      accept.classList.remove("disabled");
+    }
+  });
 
-    function set_trigger_focus() {
-        trigger.focus();
+  function set_trigger_focus() {
+    trigger.focus();
+  }
+
+  function openModal() {
+    document.body.style.overflow = "hidden";
+    dialog.showModal();
+
+    // if the terms dont need to be scrolled, enable the submit button and remove the disabled class
+    if (
+      form_agree.value === "true" ||
+      terms.scrollHeight - terms.scrollTop <= terms.clientHeight + 5
+    ) {
+      accept.disabled = false;
+      accept.classList.remove("disabled");
+    } else {
+      accept.disabled = true;
+      accept.classList.add("disabled");
     }
 
-    function openModal() {
-        document.body.style.overflow = 'hidden';
-        dialog.showModal();
+    terms.focus();
+  }
 
-        // if the terms dont need to be scrolled, enable the submit button and remove the disabled class
-        if (form_agree.value === 'true' || terms.scrollHeight - terms.scrollTop <= terms.clientHeight + 5) {
-            accept.disabled = false;
-            accept.classList.remove('disabled');
-        }
-        else {
-            accept.disabled = true;
-            accept.classList.add('disabled');
-        }
-
-        terms.focus();
-    }
-
-    function closeModal() {
-        document.body.style.overflow = 'auto';
-        dialog.close();
-        set_trigger_focus();
-    }
+  function closeModal() {
+    document.body.style.overflow = "auto";
+    dialog.close();
+    set_trigger_focus();
+  }
 })();
