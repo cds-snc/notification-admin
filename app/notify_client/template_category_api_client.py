b1e532efc87dc9a7413c77df6dde204de8436284
Andrew Leith <andrew.leith@cds-snc.ca>
2024-06-13 13:34:51 +0000

    task: add more fields to test data

--- app/notify_client/template_category_api_client.py
+++ app/notify_client/template_category_api_client.py
@@ -6,41 +6,65 @@ cats = [
         "title": "Status updates",
         "hint": "Notice of change in status, progress of a submission",
         "id": "1",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Promotional call to action",
         "hint": "Surveys, general apply now, learn more",
         "id": "2",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Service related requests",
         "hint": "Submit additional documents, follow up to move a process forward",
         "id": "3",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Fulfillment with attachments - email only",
         "hint": "Here’s your permit",
         "id": "4",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Broadcast messages",
         "hint": "General information, not related to transactions such as COVID 19 information",
         "id": "5",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Auto-reply",
         "hint": "No-reply messages, acknowledgements, response wait times",
         "id": "6",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Verification message",
         "hint": "Authentication codes, confirming an account change",
         "id": "7",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
     {
         "title": "Confirmation / Receipts",
         "hint": "Record of transaction, approvals",
         "id": "8",
+        "email_priority": "high",
+        "sms_priority": "low",
+        "hidden": "false"
     },
 ]
 
