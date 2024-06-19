7f61fdf71e08fcd7ea631f7ae1586d48be66e852
Andrew <andrew.leith@cds-snc.ca>
2024-06-19 15:05:09 -0300

    Terms of use dialog (create account) (#1856)
    * feat(tou dialog): creates a dialog component to display TOU
    
    * feat(register): add tou dialog to register page
    
    * chore: formatting
    
    * feat: add status and update when agree terms agree
    
    * chore: put js back in its own file; regen js/css
    
    * chore: formatting
    
    * fix: undo accidental change to cypress config
    
    * chore: refactor task-list a bit to make it easier to use on the register form
    
    * feat(register form): add new hidden field for agreeing to the terms
    
    * feat(validation summary): don't display hidden fields since a user can't fix them
    
    * feat(register form): add hidden field to HTML page
    
    * feat(tou dialog): refactor to make use of list-item; finish work on making it work as a form control
    
    * a11y(register): explicitly set the action location so that the form doesn't post to #anchors after form validation occurs.
    
    * chore: formatting
    
    * fix(validation_summary test): add tou agree field to tests
    
    * fix: update more tests that depend on the register user form
    
    * chore: formatting
    
    * task: add `testid`'s
    
    * task: add ui tests
    
    * task: fix tests
    
    * chore: formatting
    
    * task: update ui test; include a11y suite
    
    * feat: add feature flag
    
    * chore: refactor terms into a macro; move terms to its own non-gca page
    
    * a11y: fix invalid html
    
    * chore: formatting
    
    * chore: update translations
    
    * Update app/assets/stylesheets/tailwind/components/tou-dialog.css
    
    * style: fix up styles per review comments
    
    * style: more style changes based on review
    
    * fix: change test order
    
    * chore: regen css/js
    
    * chore: refactor terms macro to allow headings to be accessed
    
    * chore: update translations
    
    * fix: remove unneeded call to `cy.clearCookie`; tests are isolated so this isn't needed and may produce side-effects
    
    * chore: regen resources
    
    * fix: remove duplicate FR entry likely due to bad merge

--- app/assets/javascripts/fontawesome.js
+++ app/assets/javascripts/fontawesome.js
@@ -15,6 +15,7 @@ import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons/faCircleQues
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons/faTriangleExclamation";
 import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons/faCircleExclamation";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons/faXMark";
 
 let FontAwesomeIconLoader = () => {
   config.autoAddCss = false;
@@ -28,6 +29,7 @@ let FontAwesomeIconLoader = () => {
     faTriangleExclamation,
     faCircleExclamation,
     faInfoCircle,
+    faXmark,
   ]);
   dom.watch();
 };
---
+++ tests_cypress/cypress/e2e/admin/tou_dialog.cy.js
@@ -0,0 +1,139 @@
+import RegisterPage from "../../Notify/Admin/Pages/RegisterPage";
+
+describe("TOU Dialog", () => {
+  beforeEach(() => {
+    cy.visit("/register");
+  });
+
+  context("A11Y", () => {
+    it("Is accessible and has valid HTML", () => {
+      cy.a11yScan("/register", {
+        a11y: true,
+        htmlValidate: true,
+        deadLinks: false,
+        mimeTypes: false,
+      });
+    });
+
+    it("Has focus set to terms when dialog opens", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Components.TOUTerms().should("have.focus");
+    });
+
+    it("Has a trigger that is described by its status", () => {
+      RegisterPage.Components.TOUTrigger()
+        .should("have.attr", "aria-describedby")
+        .then((describedById) => {
+          cy.get("#" + describedById).should("be.visible");
+        });
+    });
+
+    it("Has terms that are described by the heading", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Components.TOUTerms()
+        .should("have.attr", "aria-describedby")
+        .then((labelledById) => {
+          cy.get("#" + labelledById).should("be.visible");
+        });
+    });
+
+    it("Closes when user presses ESC", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      cy.get("body").type("{esc}");
+      RegisterPage.Components.TOUDialog().should("not.be.visible");
+      RegisterPage.Components.TOUTrigger().should("have.focus");
+    });
+
+    it("Has scrollable terms that are also focusable", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Components.TOUTerms().should("have.focus");
+      RegisterPage.Components.TOUTerms().then(($el) => {
+        expect($el[0].scrollHeight).to.be.gt($el[0].clientHeight);
+      });
+    });
+  });
+
+  context("Trigger", () => {
+    it("Shows as not-complete on initial load", () => {
+      RegisterPage.Components.TOUStatusNotComplete().should("be.visible");
+    });
+
+    it("Remains as not-complete if user opens the dialog and clicks [cancel, ESC, close]", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Close();
+      RegisterPage.Components.TOUStatusNotComplete().should("be.visible");
+
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Cancel();
+      RegisterPage.Components.TOUStatusNotComplete().should("be.visible");
+
+      RegisterPage.Components.TOUTrigger().click();
+      cy.get("body").type("{esc}");
+      RegisterPage.Components.TOUStatusNotComplete().should("be.visible");
+    });
+
+    it("Updates to complete after clicking agree", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.AgreeToTerms();
+      RegisterPage.Components.TOUStatusComplete().should("be.visible");
+    });
+
+    it("Displays error message/error summary if terms not agreed and user submits form", () => {
+      RegisterPage.Continue();
+      RegisterPage.Components.TOUErrorMessage().should("be.visible");
+      RegisterPage.Components.TOUValidationSummaryErrorMessage().should(
+        "be.visible",
+      );
+    });
+
+    it("Error message/summary is removed after agreeing to terms and submitting the form", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.AgreeToTerms();
+      RegisterPage.Continue();
+      RegisterPage.Components.TOUErrorMessage().should("not.exist");
+      RegisterPage.Components.TOUValidationSummaryErrorMessage().should(
+        "not.exist",
+      );
+    });
+
+    it("Can click on error summary and land on dialog trigger", () => {
+      RegisterPage.Continue();
+      RegisterPage.Components.TOUValidationSummaryErrorMessage().should(
+        "be.visible",
+      );
+      RegisterPage.Components.TOUValidationSummaryErrorMessage().click();
+      RegisterPage.Components.TOUTrigger().should("be.visible");
+    });
+  });
+
+  context("Dialog", () => {
+    //     - Dialog opens when trigger is clicked
+    it("Opens when trigger is clicked", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Components.TOUDialog().should("be.visible");
+    });
+
+    it("Has agree button disabled when dialog opens", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Components.TOUAgree().should("be.disabled");
+    });
+
+    it("Enables agree button when user scrolls to bottom of terms", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.ScrollTerms();
+      RegisterPage.Components.TOUAgree().should("not.be.disabled");
+    });
+
+    it("Closes when user clicks close/cancel", () => {
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Cancel();
+      RegisterPage.Components.TOUDialog().should("not.be.visible");
+      RegisterPage.Components.TOUTrigger().should("have.focus");
+
+      RegisterPage.Components.TOUTrigger().click();
+      RegisterPage.Close();
+      RegisterPage.Components.TOUDialog().should("not.be.visible");
+      RegisterPage.Components.TOUTrigger().should("have.focus");
+    });
+  });
+});
