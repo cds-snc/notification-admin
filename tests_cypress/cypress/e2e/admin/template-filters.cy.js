78c6308e3b43b18b67ffa0e48b60bcf8fe2638ae
Andrew Leith <andrew.leith@cds-snc.ca>
2024-07-19 13:04:08 +0000

    fix: run all template filter tests in both english and french

--- tests_cypress/cypress/e2e/admin/template-filters.cy.js
+++ tests_cypress/cypress/e2e/admin/template-filters.cy.js
@@ -18,49 +18,48 @@ describe("Template filters", () => {
     cy.login(Cypress.env("NOTIFY_USER"), Cypress.env("NOTIFY_PASSWORD"));
   });
 
-  // Filters should be set to all by default
-  it("should be collapsed and set to all by default", () => {
-    cy.visit(`/services/${config.Services.Cypress}/templates`);
-
-    // ensure the first type filter is active by default and no others are
-    Page.Components.TypeFilter()
-      .find("a")
-      .first()
-      .should("have.class", "active");
-    Page.Components.TypeFilter()
-      .find("a")
-      .not(":first")
-      .should("not.have.class", "active");
-
-    // ensure the first category filter is active by default and no others are
-    Page.Components.CategoryFilter()
-      .find("a")
-      .first()
-      .should("have.class", "active");
-    Page.Components.CategoryFilter()
-      .find("a")
-      .not(":first")
-      .should("not.have.class", "active");
-
-    Page.Components.Filter().should("not.have.attr", "open");
-  });
-
-  it("should toggle", () => {
-    cy.visit(`/services/${config.Services.Cypress}/templates`);
-
-    Page.ToggleFilters();
-    Page.Components.Filter().should("have.attr", "open");
-
-    Page.ToggleFilters();
-    Page.Components.Filter().should("not.have.attr", "open");
-  });
-  // Filter by email and assert the correct number of templates are shown
   ["en", "fr"].forEach((lang) => {
     const url =
       lang == "en"
         ? `/services/${config.Services.Cypress}/templates`
         : `/set-lang?from=/services/${config.Services.Cypress}/templates`;
-    context(`Filtering in ${lang}`, () => {
+    context(`App language: ${lang.toUpperCase()}`, () => {
+      it("should be collapsed and set to all by default", () => {
+        cy.visit(url);
+
+        // ensure the first type filter is active by default and no others are
+        Page.Components.TypeFilter()
+          .find("a")
+          .first()
+          .should("have.class", "active");
+        Page.Components.TypeFilter()
+          .find("a")
+          .not(":first")
+          .should("not.have.class", "active");
+
+        // ensure the first category filter is active by default and no others are
+        Page.Components.CategoryFilter()
+          .find("a")
+          .first()
+          .should("have.class", "active");
+        Page.Components.CategoryFilter()
+          .find("a")
+          .not(":first")
+          .should("not.have.class", "active");
+
+        Page.Components.Filter().should("not.have.attr", "open");
+      });
+
+      it("should toggle", () => {
+        cy.visit(url);
+
+        Page.ToggleFilters();
+        Page.Components.Filter().should("have.attr", "open");
+
+        Page.ToggleFilters();
+        Page.Components.Filter().should("not.have.attr", "open");
+      });
+
       context("Filtering by type", () => {
         types[lang].forEach((type) => {
           it(`${type}: displays the correct number of rows`, () => {
