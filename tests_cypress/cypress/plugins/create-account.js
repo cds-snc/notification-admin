7971d88672f13894c0b1b3b9bfc4d0dc6341f561
Andrew Leith <andrew.leith@cds-snc.ca>
2024-07-22 09:29:57 -0300

    feat: new plugin to generate a random email alias, then call an endpoint on the API to create a UI testing account with it

---
+++ tests_cypress/cypress/plugins/create-account.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const https = require('https');
+
+// TODO: This duplicates some code in Notify/NotifyAPI.js and should be consolidated
+const Utilities = {
+    CreateJWT: (username, secret) => {
+        const jwt = require('jsrsasign');
+        const claims = {
+            'iss': username,
+            'iat': Math.round(Date.now() / 1000)
+        }
+
+        const headers = { alg: "HS256", typ: "JWT" };
+        return jwt.jws.JWS.sign("HS256", JSON.stringify(headers), JSON.stringify(claims), secret);
+    },
+    GenerateID: (length = 10) => {
+        const characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+};
+
+const createAccount = async (baseUrl, username, secret) => {
+    // return a generated id
+    const token = Utilities.CreateJWT(username, secret);
+    const generatedUsername = Utilities.GenerateID(10);
+    const url = `${baseUrl}/cypress/create_user/${generatedUsername}`;
+    console.log('generatedUsername', generatedUsername);
+
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET', // or 'POST', depending on your API
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": 'application/json'
+            }
+        };
+
+        const getHttpModule = (url) => url.startsWith('https://') ? https : http;
+
+        const req = getHttpModule(url).request(url, options, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(data);
+                    resolve(parsedData);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error); // Reject the promise on request error
+        });
+
+        req.end();
+    });
+
+};
+
+
+module.exports = createAccount;
