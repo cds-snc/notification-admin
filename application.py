599652fbe0fd89e0f0ac56a99ee66d66d535b35b
Michael Pond <michael.pond@cds-snc.ca>
2024-08-06 18:19:31 +0000

    attempting Custom TEmplate loader

--- application.py
+++ application.py
@@ -5,15 +5,27 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from dotenv import load_dotenv
 from flask import Flask
+from jinja2 import FileSystemLoader
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
 
+class CustomLoader(FileSystemLoader):
+    def load(self, environment, name, globals=None):
+        template = super().load(environment, name, globals)
+        template.name = name
+        return template
+
+
 load_dotenv()
 
 application = Flask("app")
 application.wsgi_app = ProxyFix(application.wsgi_app)  # type: ignore
+
+# Use the custom loader
+custom_loader = CustomLoader(searchpath='app/templates')
+application.jinja_loader = custom_loader
 
 create_app(application)
 
@@ -35,3 +47,4 @@ if os.environ.get("USE_LOCAL_JINJA_TEMPLATES") == "True":
 
 def handler(event, context):
     return apig_wsgi_handler(event, context)
+
