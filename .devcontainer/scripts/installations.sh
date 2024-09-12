4661f7936ddb5837051e71f3c47cb7f3be69081c
William B <7444334+whabanks@users.noreply.github.com>
2024-09-12 15:32:32 -0400

    Add Monkeytype and Poe the Poet plugin for Poetry (#1731)
    * Add monkeytype and poe the poet plugin for poetry
    
    * Fix some wording
    
    * Fix typo in trace-tests task
    
    * Improve poe tasks
    
    - Added monkeytype config
    - Specified code filter so we only trace packages and modules we actually want traced
    
    * Only allow monkeytype to trace our modules
    
    - Fixed typos in Poe tasks
    
    * Update lock file
    
    * Refresh lock file
    
    * Formatting
    
    * Update build backend
    
    - Install wheel before awslambdaric
    
    * Update pip before running installs
    
    * Quick typo fix
    
    * Messing with build deps
    
    * Test updated alpine image
    
    * Migrate make targets to poe
    
    * Update poetry lock file
    
    * Fix Dockerfile.lambda
    
    - Added rust and cargo to fix failing wheel build
    
    * Add rust and cargo to Dockerfile
    
    * Bump docker image to 3.18
    
    - these images are going to be the end of me...
    
    * Update lock file
    
    * update lock file
    
    ---------
    
    Co-authored-by: Jumana B <jzbahrai@users.noreply.github.com>

--- .devcontainer/scripts/installations.sh
+++ .devcontainer/scripts/installations.sh
@@ -11,6 +11,7 @@ echo -e "alias ls='exa'" >> ~/.zshrc
 echo -e "alias l='exa -alh'" >> ~/.zshrc
 echo -e "alias ll='exa -alh@ --git'" >> ~/.zshrc
 echo -e "alias lt='exa -al -T -L 2'" >> ~/.zshrc
+echo -e "alias poe='poetry run poe'" >> ~/.zshrc
 
 # Poetry autocomplete
 echo -e "fpath+=/.zfunc" >> ~/.zshrc
@@ -32,6 +33,10 @@ poetry completions zsh > ~/.zfunc/_poetry
 # Install dependencies
 cd /workspaces/notification-admin
 poetry install
+
+# Poe the Poet plugin tab completions
+touch ~/.zfunc/_poe
+poetry run poe _zsh_completion > ~/.zfunc/_poe
 
 npm rebuild node-sass
 make generate-version-file
