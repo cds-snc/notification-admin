864a4f5894a59f569cf2da2fb6e353f1a95ca637
Stephen Astels <sastels@gmail.com>
2024-10-04 17:18:36 -0400

    add more details

--- tests/app/main/views/test_find_ids.py
+++ tests/app/main/views/test_find_ids.py
@@ -27,7 +27,7 @@ def test_find_ids_displays_notifications_found(client_request, platform_admin_us
     client_request.login(platform_admin_user)
     get_records = mocker.patch(
         "app.support_api_client.find_ids",
-        return_value=[{"id": "1234", "type": "notification", "service_id": "service_1234"}],
+        return_value=[{"id": "1234", "type": "notification", "service_id": "service_1234", "service_name": "Test Service", "template_id": "template_1234", "template_name": "Test Template"}],
     )
     document = client_request.post(
         "main.find_ids",
@@ -35,16 +35,21 @@ def test_find_ids_displays_notifications_found(client_request, platform_admin_us
         _expected_status=200,
     )
     get_records.assert_called_once_with("1234")
-    result = document.find("a", {"class": "browse-list-link"})
-    assert result.text.strip() == "notification"
-    assert result.attrs["href"] == "/services/service_1234/notification/1234"
+    results = document.findAll("a", {"class": "browse-list-link"})
+    assert len(results) == 3
+    assert results[0].text.strip() == "Test Service"
+    assert results[0].attrs["href"] == "/services/service_1234"
+    assert results[1].text.strip() == "Test Template"
+    assert results[1].attrs["href"] == "/services/service_1234/templates/template_1234"
+    assert results[2].text.strip() == "notification"
+    assert results[2].attrs["href"] == "/services/service_1234/notification/1234"
 
 
 def test_find_ids_displays_templates_found(client_request, platform_admin_user, mocker):
     client_request.login(platform_admin_user)
     get_records = mocker.patch(
         "app.support_api_client.find_ids",
-        return_value=[{"id": "1234", "type": "template", "template_name": "Test Template", "service_id": "service_1234"}],
+        return_value=[{"id": "1234", "type": "template", "template_name": "Test Template", "service_id": "service_1234", "service_name": "Test Service"}],
     )
     document = client_request.post(
         "main.find_ids",
@@ -52,9 +57,12 @@ def test_find_ids_displays_templates_found(client_request, platform_admin_user, 
         _expected_status=200,
     )
     get_records.assert_called_once_with("1234")
-    result = document.find("a", {"class": "browse-list-link"})
-    assert result.text.strip() == "Test Template"
-    assert result.attrs["href"] == "/services/service_1234/templates/1234"
+    results = document.findAll("a", {"class": "browse-list-link"})
+    assert len(results) == 2
+    assert results[0].text.strip() == "Test Service"
+    assert results[0].attrs["href"] == "/services/service_1234"
+    assert results[1].text.strip() == "Test Template"
+    assert results[1].attrs["href"] == "/services/service_1234/templates/1234"
 
 
 def test_find_ids_displays_users_found(client_request, platform_admin_user, mocker):
@@ -79,7 +87,7 @@ def test_find_ids_displays_jobs_found(client_request, platform_admin_user, mocke
     client_request.login(platform_admin_user)
     get_records = mocker.patch(
         "app.support_api_client.find_ids",
-        return_value=[{"id": "1234", "type": "job", "service_id": "service_1234"}],
+        return_value=[{"id": "1234", "type": "job", "service_id": "service_1234", "service_name": "Test Service", "template_id": "template_1234", "template_name": "Test Template"}],
     )
     document = client_request.post(
         "main.find_ids",
@@ -87,9 +95,14 @@ def test_find_ids_displays_jobs_found(client_request, platform_admin_user, mocke
         _expected_status=200,
     )
     get_records.assert_called_once_with("1234")
-    result = document.find("a", {"class": "browse-list-link"})
-    assert result.text.strip() == "job"
-    assert result.attrs["href"] == "/services/service_1234/jobs/1234"
+    results = document.findAll("a", {"class": "browse-list-link"})
+    assert len(results) == 3
+    assert results[0].text.strip() == "Test Service"
+    assert results[0].attrs["href"] == "/services/service_1234"
+    assert results[1].text.strip() == "Test Template"
+    assert results[1].attrs["href"] == "/services/service_1234/templates/template_1234"
+    assert results[2].text.strip() == "job"
+    assert results[2].attrs["href"] == "/services/service_1234/jobs/1234"
 
 
 def test_find_ids_display_two_records(client_request, platform_admin_user, mocker):
