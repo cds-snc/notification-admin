fda3079ecc617f40122e41f9128611590b0607b8
Andrew Leith <andrew.leith@cds-snc.ca>
2024-10-08 17:14:35 +0000

    feat(rtl template): refactor template content into a macro that will handle the RTL switch

---
+++ app/assets/javascripts/templateContent.js
@@ -0,0 +1,17 @@
+/*
+ * This module enhances the template content text area with a switch to enable right-to-left text direction.
+ */ 
+(function () {
+  const checkbox = document.getElementById('text_direction_rtl');
+  const content = document.getElementById('template_content');
+
+  // update the dir attribute when checkbox is clicked
+  checkbox.addEventListener('change', function () {
+      content.dir = this.checked ? 'rtl' : 'ltr'; 
+  });
+
+  // on page load, if the checkbox is checked, set the dir attribute
+  if (checkbox.checked) {
+      content.dir = 'rtl';
+  }
+})()
