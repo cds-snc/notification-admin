c8927e4be62af9d00cfca1f2e9722d332fb4c437
Stephen McMurtry <smcmurtry@gmail.com>
2025-02-19 19:58:37 +0000

    update en content

--- app/main/views/manage_users.py
+++ app/main/views/manage_users.py
@@ -176,7 +176,7 @@ def remove_user_from_service(service_id, user_id):
         # redirect to the "your services" page
         flash(
             [
-                _l("You have left “{}”").format(current_service.name),
+                _l("You're no longer on the team for “{}”").format(current_service.name),
             ],
             "default_with_tick",
         )
