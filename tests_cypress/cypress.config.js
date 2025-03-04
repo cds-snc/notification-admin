308d0e76c1e12cdf6a47f42532545a818b27376e
Andrew Leith <andrew.leith@cds-snc.ca>
2025-03-04 13:15:16 +0000

    task: remove config.js; move static IDS to `cypress.config.js`; add helper utilities to retrieve values

--- tests_cypress/cypress.config.js
+++ tests_cypress/cypress.config.js
@@ -1,5 +1,3 @@
-var config = require('./config');
-
 const { defineConfig } = require("cypress");
 const EmailAccount = require("./cypress/plugins/email-account");
 const CreateAccount = require("./cypress/plugins/create-account");
@@ -7,8 +5,44 @@ const htmlvalidate = require("cypress-html-validate/plugin");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: config.Hostnames.Admin,
+    // static values for all environments
+    env: {
+      CYPRESS_SERVICE_ID: 'd4e8a7f4-2b8a-4c9a-8b3f-9c2d4e8a7f4b',
+      Services: {
+        GCNotify: 'd6aa2c68-a2d9-4437-ab19-3ae8eb202553'
+      },
+      Templates: {
+        'SMOKE_TEST_EMAIL': 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+        'SMOKE_TEST_SMS': 'e4b8f8d0-6a3b-4b9e-8c2b-1f2d3e4a5b6c',
+      },
+      Environment: {
+        STAGING: {
+          Hostnames: {
+            API: 'https://api.staging.notification.cdssandbox.xyz',
+            Admin: 'https://staging.notification.cdssandbox.xyz',
+            DDAPI: 'https://api.document.staging.notification.cdssandbox.xyz',
+          },
+        },
+        LOCAL: {
+          Hostnames: {
+            API: 'http://localhost:6011',
+            Admin: 'http://localhost:6012',
+            DDAPI: 'http://localhost:7000',
+          },
+        }
+      }
+    },
     setupNodeEvents: async (on, config) => {
+      // Set baseUrl dynamically based on ENV
+      const envName = config.env.ENV || 'LOCAL';
+      // add error handling if config.env.ENV value doesnt exist in config.env.Environment
+      if (!config.env.Environment[envName]) {
+        throw new Error(`Environment configuration for '${envName}' does not exist.  Check the 'ENV' value in the cypress.env.json and ensure a configuration exists for that environment.`);
+      }
+
+      config.baseUrl = config.env.Environment[envName].Hostnames.Admin;
+
+
       htmlvalidate.install(on, {
         rules: {
           "form-dup-name": "off",
@@ -45,7 +79,7 @@ module.exports = defineConfig({
             let acct = CreateAccount(baseUrl, username, secret);
             global.acct = acct;
             return acct
-          }          
+          }
         },
         getUserName() {
           return global.acct;
