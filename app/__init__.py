5ab4df31493f17970c1273cde1cf1db7d8d7e517
Stephen McMurtry <smcmurtry@gmail.com>
2025-03-26 19:30:52 +0000

    got the api client working

--- app/__init__.py
+++ app/__init__.py
@@ -78,6 +78,7 @@ from app.notify_client.org_invite_api_client import org_invite_api_client
 from app.notify_client.organisations_api_client import organisations_client
 from app.notify_client.platform_stats_api_client import platform_stats_api_client
 from app.notify_client.provider_client import provider_client
+from app.notify_client.reports_api_client import reports_api_client
 from app.notify_client.service_api_client import service_api_client
 from app.notify_client.status_api_client import status_api_client
 from app.notify_client.support_api_client import support_api_client
@@ -164,6 +165,7 @@ def create_app(application):
         organisations_client,
         platform_stats_api_client,
         provider_client,
+        reports_api_client,
         service_api_client,
         status_api_client,
         support_api_client,
