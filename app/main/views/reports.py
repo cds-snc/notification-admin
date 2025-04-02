f854075a8d877877cc3a3f203040fea0f0ff2fd0
Stephen McMurtry <smcmurtry@gmail.com>
2025-04-02 15:42:15 -0400

    Add report language to the api request (#2131)
    This commit sends the language parameter to the api endpoint that creates new reports.

--- app/main/views/reports.py
+++ app/main/views/reports.py
@@ -1,10 +1,11 @@
 from flask import (
+    current_app,
     flash,
     render_template,
 )
 from flask_login import current_user
 
-from app import reports_api_client
+from app import get_current_locale, reports_api_client
 from app.main import main
 from app.utils import user_is_platform_admin
 
@@ -19,7 +20,8 @@ def reports(service_id):
 @main.route("/services/<service_id>/reports", methods=["post"])
 @user_is_platform_admin
 def generate_report(service_id):
-    reports_api_client.request_report(user_id=current_user.id, service_id=service_id, report_type="email")
+    language = get_current_locale(current_app)
+    reports_api_client.request_report(user_id=current_user.id, service_id=service_id, report_type="email", language=language)
     flash("Test report has been requested", "default")
     reports = reports_api_client.get_reports_for_service(service_id)
     return render_template("views/reports/reports.html", reports=reports)
