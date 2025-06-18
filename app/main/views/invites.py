ca08551d20d73825083571520a0e4d1965be4e60
Jumana B <jzbahrai@users.noreply.github.com>
2025-06-18 15:16:35 -0400

    Return 200 for HEAD requests to `/invitation/<token>` (#2197)
    Co-authored-by: Andrew <andrew.leith@cds-snc.ca>
    Co-authored-by: Stephen McMurtry <smcmurtry@gmail.com>

--- app/main/views/invites.py
+++ app/main/views/invites.py
@@ -1,6 +1,6 @@
 from urllib.error import HTTPError
 
-from flask import abort, current_app, flash, redirect, render_template, session, url_for
+from flask import abort, current_app, flash, redirect, render_template, request, session, url_for
 from flask_babel import _
 from flask_login import current_user
 from markupsafe import Markup
@@ -14,6 +14,9 @@ from app.notify_client import InviteTokenError
 
 @main.route("/invitation/<token>")
 def accept_invite(token):
+    # For HEAD requests, just return a 200 OK without processing the invitation
+    if request.method == "HEAD":
+        return "", 200
     try:
         invited_user = InvitedUser.from_token(token)
     except InviteTokenError as exception:
