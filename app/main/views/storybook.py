61db93aa0037bf350a6aac5ba166cccad290e6b3
Andrew Leith <andrew.leith@gmail.com>
2025-07-08 17:44:31 +0000

    chore: fix duplicate ids in storybook

--- app/main/views/storybook.py
+++ app/main/views/storybook.py
@@ -1,7 +1,44 @@
 from flask import render_template, request
+from flask_wtf import FlaskForm as Form
+from wtforms import RadioField
 
 from app.main import main
-from app.main.forms import AuthMethodForm
+
+
+class Exampleform1(Form):
+    auth_method1 = RadioField("Select your two-step verification method")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auth_method1.choices = [
+            ("email-1", "Recieve a code by email"),
+            ("sms-1", "Recieve a code by text message"),
+            ("new_key-1", "Add a new security key"),
+        ]
+
+
+class Exampleform2(Form):
+    auth_method2 = RadioField("Select your two-step verification method")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auth_method2.choices = [
+            ("email-2", "Recieve a code by email"),
+            ("sms-2", "Recieve a code by text message"),
+            ("new_key-2", "Add a new security key"),
+        ]
+
+
+class Exampleform3(Form):
+    auth_method3 = RadioField("Select your two-step verification method")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auth_method3.choices = [
+            ("email-3", "Recieve a code by email"),
+            ("sms-3", "Recieve a code by text message"),
+            ("new_key-3", "Add a new security key"),
+        ]
 
 
 @main.route("/_storybook")
@@ -11,23 +48,8 @@ def storybook():
         component = request.args["component"]
 
     # TODO: don't pass forms to every single component
-    data1 = [
-        ("email-1", "Recieve a code by email"),
-        ("sms-1", "Recieve a code by text message"),
-        ("new_key-1", "Add a new security key"),
-    ]
-    data2 = [
-        ("email-2", "Recieve a code by email"),
-        ("sms-2", "Recieve a code by text message"),
-        ("new_key-2", "Add a new security key"),
-    ]
-    data3 = [
-        ("email-3", "Recieve a code by email"),
-        ("sms-3", "Recieve a code by text message"),
-        ("new_key-3", "Add a new security key"),
-    ]
-    form1 = AuthMethodForm(all_auth_methods=data1, current_auth_method="current_auth_method")
-    form2 = AuthMethodForm(all_auth_methods=data2, current_auth_method="current_auth_method")
-    form3 = AuthMethodForm(all_auth_methods=data3, current_auth_method="current_auth_method")
+    form1 = Exampleform1()
+    form2 = Exampleform2()
+    form3 = Exampleform3()
 
     return render_template("views/storybook.html", component=component, form1=form1, form2=form2, form3=form3)
