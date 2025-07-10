13a570824a9907026de6ffdd8d69116093872bfd
Andrew Leith <andrew.leith@gmail.com>
2025-07-10 13:23:32 +0000

    feat(2fa badge): add verification badges to 2fa screen

--- app/main/views/user_profile.py
+++ app/main/views/user_profile.py
@@ -386,16 +386,28 @@ def user_profile_2fa():
         data = [
             ("email", _("Receive a code by email")),
             ("sms", _("Receive a code by text message")),
+            *[(f"key_{key['id']}", _("Use '{}' key").format(key["name"])) for key in getattr(current_user, "security_keys", [])],
             ("new_key", _("Add a new security key")),
-            # todo: add options for existing security keys
         ]
         hints = {
             "email": current_user.email_address,
-            "sms": current_user.mobile_number,
+            "sms": current_user.mobile_number
+            if current_user.mobile_number
+            else _("Add a mobile number to your profile to use this option."),
             "new_key": _(
                 "Enhance your account’s security by adding a security key such as a government issued Yubi key. Follow prompts to add the key."
             ),
         }
+        badge_options = {
+            "email": ["Verified", "success"],
+            "sms": None
+            if current_user.mobile_number is None
+            else ["Not verified", "default"]
+            if not current_user.verified_phonenumber
+            else ["Verified", "success"],
+            "new_key": None,
+        }
+
         if current_user.auth_type == "email_auth":
             current_auth_method = "email"
         elif current_user.auth_type == "sms_auth":
@@ -424,10 +436,10 @@ def user_profile_2fa():
             current_user.update(auth_type=auth_type)
 
             # Flash a success message
-            flash(_("Two-factor authentication method updated"), "default_with_tick")
+            flash(_("Two-step verification method updated"), "default_with_tick")
 
             # Redirect back to user profile
             return redirect(url_for(".user_profile"))
 
-        return render_template("views/user-profile/2fa.html", form=form, hints=hints)
+        return render_template("views/user-profile/2fa.html", form=form, hints=hints, badge_options=badge_options)
     return redirect(url_for(".user_profile"))
