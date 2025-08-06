7cd4ef69976189249d0da7b8cb7330c00ebe85c7
Philippe Caron <philippe.caron@cds-snc.ca>
2025-08-06 17:36:25 +0000

    Provide feedback from sanitize-pii module

--- app/assets/javascripts/contactSanitizePii.js
+++ app/assets/javascripts/contactSanitizePii.js
@@ -8,6 +8,31 @@
     : null;
 
   if (contactForm && contactMessage) {
+    // Listen for input changes and display a warning listing PII items found. 
+    contactMessage.addEventListener("input", function () {
+      const sanitizedMessage = sanitizePii(contactMessage.value,{ detectOnly: true });
+      const piiItems = JSON.parse(sanitizedMessage);
+      if (piiItems.length) {
+        // Add a list item for each PII item detected in #sanitize-pii empty UL
+        const piiList = document.querySelector("#sanitize-pii ul");
+        piiList.innerHTML = ""; // Clear previous items
+        piiItems.forEach(function (item) {
+          const listItem = document.createElement("li");
+          listItem.textContent = `${window.polyglot.t(item.pattern)}: ${item.match}`;
+          piiList.appendChild(listItem);
+        });
+        // Show the PII warning div
+        document.querySelector("#sanitize-pii").style.display = "block";
+      } else {
+        // Hide the PII warning div if no PII items are detected
+        document.querySelector("#sanitize-pii").style.display = "none";
+      }
+      // Log the PII items to the console for debugging
+      if (piiItems.length) {
+        console.warn("PII items detected in the message:", piiItems);
+      }
+    });
+    // Listen for form submission
     contactForm.addEventListener("submit", function () {
       try {
         contactMessage.value = sanitizePii(contactMessage.value);
