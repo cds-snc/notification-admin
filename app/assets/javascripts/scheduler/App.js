940ad33ad3d5b04137577f84568867f513d9a488
Andrew Leith <andrew.leith@gmail.com>
2025-09-15 14:16:32 +0000

    chore: remove unused import

--- app/assets/javascripts/scheduler/App.js
+++ app/assets/javascripts/scheduler/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from "react";
+import React, { useContext } from "react";
 import { StateProvider, setIntialState, defaultState } from "./store";
 import { I18nProvider, I18nContext } from "./i18n";
 import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
