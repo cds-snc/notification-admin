aedb7b1cc88ea57152e0c08c43918864b7fe6a70
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2025-10-23 14:11:40 -0400

    moving import (#2451)
    * moving import
    
    * formatting

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,11 +4,12 @@ import time
 import traceback
 
 import gunicorn  # type: ignore
-import newrelic.agent  # See https://bit.ly/2xBVKBH
 
 enable_newrelic = os.getenv("ENABLE_NEW_RELIC", "False").lower() == "true"
 
 if enable_newrelic:
+    import newrelic.agent  # See https://bit.ly/2xBVKBH
+
     environment = os.environ.get("NOTIFY_ENVIRONMENT")
     newrelic.agent.initialize(environment=environment)  # noqa: E402
 
