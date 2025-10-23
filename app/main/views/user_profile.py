efdd81ffd51b3c8fc72c4dbc6f71efe4bef4b471
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2025-10-23 19:51:39 +0000

    added todo

--- app/main/views/user_profile.py
+++ app/main/views/user_profile.py
@@ -776,6 +776,8 @@ def deactivate_account_authenticate():
     form = ConfirmPasswordForm(_check_password)
 
     if form.validate_on_submit():
+        # TODO: The below has to be tested
+        # TODO: Potential try except block?
         session[HAS_AUTHENTICATED] = True
         user_api_client.suspend_user(current_user.id)
         logout_user()
