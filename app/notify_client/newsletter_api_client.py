83f16c01a7c3077430abf2ad5a75b94c8f154efb
Stephen McMurtry <smcmurtry@gmail.com>
2025-11-19 19:28:19 +0000

    first attempt

---
+++ app/notify_client/newsletter_api_client.py
@@ -0,0 +1,20 @@
+from app.notify_client import NotifyAdminAPIClient
+
+
+class NewsletterAPIClient(NotifyAdminAPIClient):
+    def create_unconfirmed_subscriber(self, email, language):
+        """
+        Create an unconfirmed newsletter subscriber.
+
+        Args:
+            email: Email address of the subscriber
+            language: Language preference (e.g., 'en' or 'fr')
+
+        Returns:
+            Response from the API
+        """
+        data = {"email": email, "language": language}
+        return self.post(url="/unconfirmed-subscriber", data=data)
+
+
+newsletter_api_client = NewsletterAPIClient()
