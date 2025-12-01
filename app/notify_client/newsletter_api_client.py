46207dc37bd3a396a22252a30ff0c9bc3c36963a
Stephen McMurtry <smcmurtry@gmail.com>
2025-12-01 20:54:58 +0000

    switch to a get endpoint

--- app/notify_client/newsletter_api_client.py
+++ app/notify_client/newsletter_api_client.py
@@ -32,8 +32,7 @@ class NewsletterAPIClient(NotifyAdminAPIClient):
         return resp
 
     def send_latest_newsletter(self, subscriber_id: str):
-        # TODO: Implement actual API endpoint
-        resp = self.post(url=f"/newsletter/send-latest/{subscriber_id}", data={})
+        resp = self.get(url=f"/newsletter/send-latest/{subscriber_id}")
         return resp
 
     @set("subscriber-{subscriber_id}")
