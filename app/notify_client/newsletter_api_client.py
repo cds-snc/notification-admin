10f14cbca68bb2b438748078bd743bd312c0bf91
wbanks <william.banks@cds-snc.ca>
2025-12-10 11:49:34 -0500

    Added some debug logging for bug investigation

--- app/notify_client/newsletter_api_client.py
+++ app/notify_client/newsletter_api_client.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from app.notify_client import NotifyAdminAPIClient
 from app.notify_client.cache import delete, set
 
@@ -35,12 +37,20 @@ class NewsletterAPIClient(NotifyAdminAPIClient):
         return resp
 
     def send_latest_newsletter(self, subscriber_id: str):
-        resp = self.get(url=f"/newsletter/send-latest/{subscriber_id}")
+        url = f"/newsletter/send-latest/{subscriber_id}"
+        current_app.logger.info(
+            f"[admin-newsletter-debug] - Sending latest newsletter to subscriber: {subscriber_id}. URL: {url}"
+        )
+
+        resp = self.get(url=url)
         return resp
 
     @set("subscriber-{subscriber_id}")
     def get_subscriber(self, subscriber_id: str):
-        resp = self.get(url=f"/newsletter/find-subscriber?subscriber_id={subscriber_id}")
+        url = f"/newsletter/find-subscriber?subscriber_id={subscriber_id}"
+        current_app.logger.info(f"[admin-newsletter-debug] - Finding subscriber {subscriber_id}. URL: {url}")
+
+        resp = self.get(url=url)
         return resp
 
 
