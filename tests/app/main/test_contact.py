031b452d071cf3a89991e67b6b6b64675a07eef9
Stephen McMurtry <smcmurtry@gmail.com>
2026-01-07 22:04:00 +0000

    fix test

--- tests/app/main/test_contact.py
+++ tests/app/main/test_contact.py
@@ -1,5 +1,5 @@
 import pytest
-from flask import url_for
+from flask import current_app, url_for
 from flask_login import current_user
 
 from tests.conftest import normalize_spaces
@@ -279,20 +279,16 @@ def test_contact_lang_url_switches_session_language(client_request):
         assert session["userlang"] == "fr"
 
 
-@pytest.mark.parametrize(
-    "lang_code, expected_url",
-    [
-        ("en", "https://forms-formulaires.alpha.canada.ca/en/id/cmk30s2qn00qox9018rhryfqa"),
-        ("fr", "https://forms-formulaires.alpha.canada.ca/fr/id/cmk30s2qn00qox9018rhryfqa"),
-    ],
-)
-def test_a11y_feedback_redirects_to_external_form(client_request, lang_code, expected_url):
+@pytest.mark.parametrize("lang_code, config_key", [("en", "A11Y_FEEDBACK_URL_EN"), ("fr", "A11Y_FEEDBACK_URL_FR")])
+def test_a11y_feedback_redirects_to_external_form(client_request, lang_code, config_key):
     """Test that selecting a11y_feedback redirects to the appropriate language feedback form"""
     client_request.logout()
 
     # Set session language to match the lang_code to avoid language sync redirect
     with client_request.session_transaction() as session:
         session["userlang"] = lang_code
+
+    expected_url = current_app.config[config_key]
 
     client_request.post(
         ".contact_lang",
