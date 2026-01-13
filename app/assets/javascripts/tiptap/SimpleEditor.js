2f4a3850cfcf41a8d4d028e8d21a357e38187070
Andrew Leith <andrew.leith@gmail.com>
2026-01-13 15:19:18 +0000

    chore: remove unused import

--- app/assets/javascripts/tiptap/SimpleEditor.js
+++ app/assets/javascripts/tiptap/SimpleEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { EditorContent, useEditor } from "@tiptap/react";
 import MenuBar from "./MenuBar";
 
