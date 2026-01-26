e08fb5fd60f7fc77507802297bcfabba02c2e31b
Andrew Leith <andrew.leith@gmail.com>
2026-01-26 10:07:14 -0400

    fix: remove uncaught exception handler to ensure test failures are reported

--- tests_cypress/cypress/support/e2e.js
+++ tests_cypress/cypress/support/e2e.js
@@ -33,9 +33,3 @@ before(() => {
     }).as('allRequests');
     
 });
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-});
