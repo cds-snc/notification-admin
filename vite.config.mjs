8ee2bfdf9974ed61fe76080da98f6803d9badf66
Philippe Caron <philippe.caron@cds-snc.ca>
2026-03-31 15:42:45 +0000

    Experimenting with Tailwind 4 and focus system

---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    outDir: 'app/static/stylesheets',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        index: './app/assets/stylesheets/tailwind/style.css',
+        tooltip: './app/assets/javascripts/tiptap/tooltip.css',
+        editor: './app/assets/javascripts/tiptap/editor.css',
+      },
+      output: {
+        assetFileNames: '[name].[ext]',
+      },
+    },
+    minify: false,
+  },
+})
