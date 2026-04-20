a203a434d3722f6d9af57ffac7f82a757a9be0c8
Andrew Leith <andrew.leith@gmail.com>
2026-04-20 11:53:02 +0000

    fix: attempt to balance shards evenly

--- tests_cypress/cypress/e2e/admin/ci-shard-2.cy.js
+++ tests_cypress/cypress/e2e/admin/ci-shard-2.cy.js
@@ -1,5 +1,4 @@
 import "./template-filters.cy";
-import "./template-categories.cy";
 import "./template/create-template.cy";
 import "./template/edit-template.cy";
 import "./components/remaining_messages_summary.cy";
--- tests_cypress/cypress/e2e/admin/ci-shard-3.cy.js
+++ tests_cypress/cypress/e2e/admin/ci-shard-3.cy.js
@@ -2,3 +2,4 @@ import "./a11y/app_pages.cy";
 import "./menu/disclosure_menu.cy";
 import "./sitemap/sitemap.cy";
 import "./branding/all.cy";
+import "./template-categories.cy";
