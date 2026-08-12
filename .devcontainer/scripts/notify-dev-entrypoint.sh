a0104e1b4f681743ea5f5ead1b26e3d28a3c6439
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2026-08-12 20:55:13 +0000

    fix gitconfig

--- .devcontainer/scripts/notify-dev-entrypoint.sh
+++ .devcontainer/scripts/notify-dev-entrypoint.sh
@@ -7,7 +7,15 @@ set -ex
 # tools and the filesystem mount enabled should be located here.
 ###################################################################
 
+# Include host gitconfig when mounted so aliases and user preferences are available.
+if [ -f /home/vscode/.gitconfig-host ]; then
+  if ! git config --global --get-all include.path | grep -Fxq /home/vscode/.gitconfig-host; then
+    git config --global --add include.path /home/vscode/.gitconfig-host
+  fi
+fi
+
 # Tell git the workspace repository is safe, else upcoming commands will fail.
+
 git config --global --add safe.directory /workspaces/notification-admin
 git config --global --add safe.directory /workspace
 
