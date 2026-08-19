d082f679c8a97759c956619f14f3a298b565f08c
Philippe Caron <amazingphilippe@gmail.com>
2026-08-19 09:23:04 -0400

    Fix RTE ally errors (#2764)
    * - Replace missing ID reference with label.
    - Use title attribute on svg instead of aria-label.
    
    * format
    
    * Add infopane test to ci
    
    * - Actually use aria-label with a role attribute for describing svgs
    
    * dont run in ci afterall
    
    * format

--- app/assets/javascripts/tiptap/InfoPane/MultipleLanguages.js
+++ app/assets/javascripts/tiptap/InfoPane/MultipleLanguages.js
@@ -5,22 +5,30 @@ const MultipleLanguagesInfoPane = ({ t, isActive }) => {
   const pane = t.infoPane2;
 
   return (
-    <section
-      id="multiple-languages"
-      aria-labelledby="multiple-languages-title"
-      hidden={!isActive}
-    >
+    <section id="multiple-languages" aria-label={pane.label} hidden={!isActive}>
       <p>{pane.p1}</p>
       <ul className="list list-bullet ml-10">
         <li>
           {pane.li1}{" "}
-          <Icon iconNode={englishBlockIcon} aria-label={pane.li1Icon1} />{" "}
+          <Icon
+            iconNode={englishBlockIcon}
+            role="img"
+            aria-label={pane.li1Icon1}
+          />{" "}
           {pane.li1or}{" "}
-          <Icon iconNode={frenchBlockIcon} aria-label={pane.li1Icon2} />
+          <Icon
+            iconNode={frenchBlockIcon}
+            role="img"
+            aria-label={pane.li1Icon2}
+          />
         </li>
         <li>
           {pane.li2}{" "}
-          <Icon iconNode={rightToLeftIcon} aria-label={pane.li2Icon} />
+          <Icon
+            iconNode={rightToLeftIcon}
+            role="img"
+            aria-label={pane.li2Icon}
+          />
         </li>
       </ul>
     </section>
