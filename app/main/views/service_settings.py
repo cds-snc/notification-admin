aa0b9a8616770e97e93e618ccb57f37bfa89d6ff
Andrew Leith <andrew.leith@cds-snc.ca>
2026-08-24 17:56:30 +0000

    chore: remove gate around  ui attachment features

--- app/main/views/service_settings.py
+++ app/main/views/service_settings.py
@@ -71,7 +71,6 @@ from app.utils import (
     SENDING_STATUSES,
     documentation_url,
     email_safe,
-    file_attachments_enabled_for_service,
     get_logo_cdn_domain,
     get_new_default_reply_to_address,
     get_verified_ses_domains,
@@ -120,7 +119,6 @@ def service_settings(service_id: str):
         sending_domain=current_service.sending_domain or current_app.config["SENDING_DOMAIN"],
         limits=limits,
         callback_api=callback_api,
-        file_attachments_enabled_for_service=file_attachments_enabled_for_service(current_service.id),
     )
 
 
@@ -441,14 +439,9 @@ def set_sensitive_service(service_id):
 def service_switch_upload_document(service_id):
     title = _("Send files by email")
     form = ServiceOnOffSettingForm(name=title, enabled=current_service.has_permission("upload_document"))
-    if file_attachments_enabled_for_service(current_service.id):
-        help = _(
-            "Allow files to be attached to email notifications.<br>Files can be attached on the templates page or through the API<br>Learn more in the <a href='{}'>API documentation</a>."
-        ).format(documentation_url("send", section="sending-a-file-by-email"))
-    else:
-        help = _(
-            "This feature is only available when sending through the API.<br>Learn more in the <a href='{}'>API documentation</a>."
-        ).format(documentation_url("send", section="sending-a-file-by-email"))
+    help = _(
+        "Allow files to be attached to email notifications.<br>Files can be attached on the templates page or through the API<br>Learn more in the <a href='{}'>API documentation</a>."
+    ).format(documentation_url("send", section="sending-a-file-by-email"))
 
     if form.validate_on_submit():
         current_service.force_permission("upload_document", on=form.enabled.data)
