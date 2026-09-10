adba34a6e5f614838d5bc0ab5001fde23b3269ef
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2026-09-10 18:43:11 +0000

    fix english

--- app/main/forms.py
+++ app/main/forms.py
@@ -893,7 +893,7 @@ class BaseTemplateFormWithCategory(BaseTemplateForm):
 class SMSTemplateFormWithCategory(BaseTemplateFormWithCategory):
     def validate_template_content(self, field):
         if field.data and current_app.config.get("API_KEY_PREFIX", "") in field.data:
-            raise ValidationError(_l("You can not store API keys in a template."))
+            raise ValidationError(_l("You cannot store API keys in a template."))
         OnlySMSCharacters()(None, field)
 
     template_content = TextAreaField(
@@ -908,11 +908,11 @@ class SMSTemplateFormWithCategory(BaseTemplateFormWithCategory):
 class EmailTemplateFormWithCategory(BaseTemplateFormWithCategory):
     def validate_template_content(self, field):
         if field.data and current_app.config.get("API_KEY_PREFIX", "") in field.data:
-            raise ValidationError(_l("You can not store API keys in a template."))
+            raise ValidationError(_l("You cannot store API keys in a template."))
 
     def validate_subject(self, field):
         if field.data and current_app.config.get("API_KEY_PREFIX", "") in field.data:
-            raise ValidationError(_l("You can not store API keys in a template."))
+            raise ValidationError(_l("You cannot store API keys in a template."))
 
     subject = TextAreaField(_l("Subject line of the email"), validators=[DataRequired(message=_l("This cannot be empty"))])
 
