9afeb771d97d1cedbf782f5319dc46853e71ea4c
Andrew Leith <andrew.leith@cds-snc.ca>
2026-09-25 11:39:20 +0000

    Fix error handling to fall back to generic 500 page for unsupported status codes

--- app/enums.py
+++ app/enums.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+# Status codes that have a matching template in app/templates/error/
+ERROR_TEMPLATE_STATUS_CODES = (400, 401, 403, 404, 410, 413, 418, 500)
+
 
 class NotifyEnv(Enum):
     DEVELOPMENT = "development"
